bda7ff48ed650250890b8922ca99818124a28927
Paul Hofman <paulwhofman@hotmail.com>
2025-03-10 15:47:26 +0100

    Fix dropout to work with more complex models #9

--- src/probly/models/dropout.py
+++ src/probly/models/dropout.py
@@ -1,3 +1,4 @@
+import copy
 import torch
 import torch.nn as nn
 
@@ -14,26 +15,23 @@ class Dropout(nn.Module):
         self.base = base
         self.p = p
         self.model = None
-        self.convert()
+        self._convert(base)
 
     def forward(self, x, samples):
         return torch.stack([self.model(x) for _ in range(samples)], dim=1)
 
-    def convert(self):
+    def _convert(self, base):
         """
         Converts the base model to a dropout model, stored in model, by looping through all the layers
         and adding a dropout layer after each linear layer as suggested in ...
         """
-        model = nn.Sequential()
-        children = list(self.base.named_children())
-        num_linear = sum([isinstance(module, nn.Linear) for _, module in children])
-        count_dropout = 0
-        for name, module in children:
-            model.add_module(name, module)
-            if isinstance(module, nn.Linear) and count_dropout < num_linear - 1:
-                model.add_module(f"dropout_{name}", nn.Dropout(p=self.p))
-                count_dropout += 1
-        self.model = model
+        self.model = copy.deepcopy(base)
+        first = True
+        for name, module in self.model.named_modules():
+            if isinstance(module, nn.Linear):
+                if not first:
+                    setattr(self.model, name, nn.Sequential(nn.Dropout(p=self.p), module))
+            first = False
 
     def eval(self):
         """
