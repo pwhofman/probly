9e0fe67de299f82cd18a66ec9bb28bae6c0a5850
Paul Hofman <paulwhofman@hotmail.com>
2025-03-14 13:19:00 +0100

    Add distance-based measures of uncertainty, closes #26

--- src/probly/measures/distributions.py
+++ src/probly/measures/distributions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import entropy
+from scipy.optimize import minimize
 
 def total_uncertainty_entropy(probs, base=2):
     """
@@ -69,4 +70,54 @@ def aleatoric_uncertainty_variance(probs):
 def epistemic_uncertainty_variance(probs):
     probs_mean = probs.mean(axis=1, keepdims=True)
     eu = np.sum(np.mean(probs * (probs - probs_mean), axis=1), axis=1)
+    return eu
+
+def total_uncertainty_distance(probs):
+    """
+    Compute the total uncertainty using samples from a second-order distribution.
+    The measure of total uncertainty is from https://arxiv.org/pdf/2312.00995.
+    Args:
+        probs: numpy.ndarray of shape (n_instances, n_samples, n_classes)
+    Returns:
+        tu: numpy.ndarray of shape (n_instances,)
+    """
+    probs_mean = probs.mean(axis=1)
+    tu = 1 - np.max(probs_mean, axis=1)
+    return tu
+
+def aleatoric_uncertainty_distance(probs):
+    """
+    Compute the aleatoric uncertainty using samples from a second-order distribution.
+    The measure of aleatoric uncertainty is from https://arxiv.org/pdf/2312.00995.
+    Args:
+        probs: numpy.ndarray of shape (n_instances, n_samples, n_classes)
+    Returns:
+        tu: numpy.ndarray of shape (n_instances,)
+    """
+    au = 1 - np.mean(np.max(probs, axis=2), axis=1)
+    return au
+
+def epistemic_uncertainty_distance(probs):
+    """
+    Compute the epistemic uncertainty using samples from a second-order distribution.
+    The measure of epistemic uncertainty is from https://arxiv.org/pdf/2312.00995.
+    Args:
+        probs: numpy.ndarray of shape (n_instances, n_samples, n_classes)
+    Returns:
+        tu: numpy.ndarray of shape (n_instances,)
+    """
+    def fun(q, p):
+        f = np.mean(np.linalg.norm(p - q[None, :], ord=1, axis=1))
+        return f
+
+    x0 = probs.mean(axis=1)
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bounds = [(0, 1)] * probs.shape[2]
+    eu = np.empty(probs.shape[0])
+    for i in range(probs.shape[0]):
+        res = minimize(fun=fun, x0=x0[i],
+                       bounds=bounds,
+                       constraints=constraints,
+                       args=probs[i])
+        eu[i] = 0.5 * res.fun
     return eu
