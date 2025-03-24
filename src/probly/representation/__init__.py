463df05ea223868476e1569304fc1459b124cdb0
Paul Hofman <paulwhofman@hotmail.com>
2025-03-24 22:39:14 +0100

    Remove PostNet representation for now

--- src/probly/representation/__init__.py
+++ src/probly/representation/__init__.py
@@ -1,7 +1,6 @@
 from .dropout import Dropout
 from .ensemble import Ensemble
 from .evidential import Evidential
-from .postnet import PostNet
 from .subensemble import SubEnsemble
 
-__all__ = ["Dropout", "Ensemble", "Evidential", "PostNet", "SubEnsemble"]
+__all__ = ["Dropout", "Ensemble", "Evidential", "SubEnsemble"]
