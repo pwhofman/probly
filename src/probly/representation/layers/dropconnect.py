20264f1b9b85c5c45974381263304823633d2ba8
Timo <t.loehr@lmu.de>
2025-03-25 17:23:18 +0100

    Add DropConnect models as a representation

---
+++ src/probly/representation/layers/dropconnect.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DropConnectLinear(nn.Module):
+    """
+    Custom Linear layer with DropConnect applied to weights during training.
+    Args:
+        base_layer: nn.Linear, The original linear layer to be wrapped.
+        p: float, The probability of dropping individual weights.
+    """
+
+    def __init__(self, base_layer: nn.Linear, p: float = 0.25):
+        super().__init__()
+        self.in_features = base_layer.in_features
+        self.out_features = base_layer.out_features
+        self.p = p
+        self.weight = nn.Parameter(base_layer.weight.clone().detach())
+        self.bias = nn.Parameter(
+            base_layer.bias.clone().detach()) if base_layer.bias is not None else None
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the DropConnect layer.
+        Args:
+            x: torch.Tensor, input data
+        Returns:
+            torch.Tensor, layer output
+        """
+        if self.training:
+            mask = (torch.rand_like(self.weight) > self.p).float()
+            weight = self.weight * mask  # Apply DropConnect
+        else:
+            weight = self.weight * (1 - self.p)  # Scale weights at inference time
+
+        return F.linear(x, weight, self.bias)
+
+    def extra_repr(self) -> str:
+        """
+
+        """
+        return f"in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}"
