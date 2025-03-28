1202b4be3a80249cc3a5061ac675c5bc2075b8e6
Timo <t.loehr@lmu.de>
2025-03-28 09:49:45 +0100

    Clean up focal loss implementation

--- src/probly/losses.py
+++ src/probly/losses.py
@@ -113,14 +113,12 @@ class FocalLoss(nn.Module):
     Args:
         alpha: float, control importance of minority class
         gamma: float, control loss for hard instances
-        reduction: str 'mean' or 'sum' or None, method of reduction towards single loss value
     """
 
-    def __init__(self, alpha: float = 1, gamma: float = 2, reduction: str = 'mean') -> None:
+    def __init__(self, alpha: float = 1, gamma: float = 2) -> None:
         super().__init__()
         self.alpha = alpha
         self.gamma = gamma
-        self.reduction = reduction
 
     def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         """
@@ -129,20 +127,13 @@ class FocalLoss(nn.Module):
             inputs: torch.Tensor of size (n_instances, n_classes)
             targets: torch.Tensor of size (n_instances,)
         Returns:
-            loss: torch.Tensor size (1,) or (n_instances,), loss value
+            loss: torch.Tensor, mean loss value
         """
-        log_prob = F.log_softmax(inputs, dim=-1)  # TODO check other way around and test function
-        prob = torch.exp(log_prob)  # calculate model confidence
-
         targets_one_hot = F.one_hot(targets, num_classes=inputs.shape[-1])
+        prob = F.softmax(inputs, dim=-1)
         p_t = torch.sum(prob * targets_one_hot, dim=-1)
 
+        log_prob = torch.log(prob)
         loss = -self.alpha * (1 - p_t) ** self.gamma * torch.sum(log_prob * targets_one_hot, dim=-1)
 
-        # Apply reduction (mean, sum, or no reduction)
-        if self.reduction == 'mean':
-            return loss.mean()
-        elif self.reduction == 'sum':
-            return loss.sum()
-        else:
-            return loss
+        return torch.mean(loss)
