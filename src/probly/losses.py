9440a4a857caa49096a8ddb89a2ca0dce840b5e7
Paul Hofman <36850933+pwhofman@users.noreply.github.com>
2025-04-10 16:26:45 +0200

    Add Label Relaxation loss, closes #31 and closes #8 (#70)

--- src/probly/losses.py
+++ src/probly/losses.py
@@ -283,3 +283,49 @@ class ExpectedCalibrationError(nn.Module):
             weight = _bin.shape[0] / num_instances
             loss += weight * torch.abs(acc_bin - conf_bin)
         return loss
+
+
+class LabelRelaxationLoss(nn.Module):
+    """Label Relaxation Loss from https://ojs.aaai.org/index.php/AAAI/article/view/17041.
+
+    This loss is used to improve the calibration of a neural network. It works by minimizing
+    the Kullback-Leibler divergence between the predicted probabilities and the target distribution in the credal set
+    defined by the alpha parameter. The target distribution is the distribution in the credal set that minimizes the
+    Kullback-Leibler divergence from the predicted probabilities. If the predicted probability distribution
+    is in the credal set, the loss is zero.
+
+    Attributes:
+        alpha: float, the parameter that controls the amount of label relaxation. Increasing alpha, increases the size
+            of the credal set and thus the amount of label relaxation.
+    """
+
+    def __init__(self, alpha: float = 0.1) -> None:
+        """Initializes an instance of the LabelRelaxationLoss class.
+
+        Args:
+            alpha: float, the parameter that controls the amount of label relaxation.
+                Increasing alpha, increases the size of the credal set and thus the amount of label relaxation.
+        """
+        super().__init__()
+        self.alpha = alpha
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the label relaxation loss.
+
+        Args:
+            inputs: torch.Tensor of size (n_instances, n_classes)
+            targets: torch.Tensor of size (n_instances,)
+
+        Returns:
+            loss: torch.Tensor, mean loss value
+        """
+        inputs_probs = F.softmax(inputs, dim=1)
+
+        with torch.no_grad():
+            inv_one_hot = 1 - F.one_hot(targets, inputs.shape[1])
+            targets_real = self.alpha * inputs_probs / torch.sum(inv_one_hot * inputs_probs, dim=1, keepdim=True)
+            targets_real[torch.arange(targets.shape[0]), targets] = 1 - self.alpha
+
+        kl_div = torch.sum(F.kl_div(inputs_probs.log(), targets_real, log_target=False, reduction="none"), dim=1)
+        loss = torch.where(torch.sum(inv_one_hot * inputs_probs, dim=1) <= self.alpha, 0, kl_div)
+        return loss.mean()
