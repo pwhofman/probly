bdc669be80e1063e4182cb96914f4e9e937fb246
Paul Hofman <paulwhofman@hotmail.com>
2025-04-13 13:53:04 +0200

    Add sampling for the evidential regression representation

--- src/probly/representation/regression/evidential.py
+++ src/probly/representation/regression/evidential.py
@@ -81,4 +81,25 @@ class Evidential(nn.Module):
                 )
                 break
 
-    # TODO: Implement sample method
+    def sample(self, x: torch.Tensor, n_samples: int) -> torch.Tensor:
+        """Sample from the predicted distribution for a given input x.
+
+        Returns a tensor of shape (n_instances, n_samples, 2) representing the parameters of the sampled normal
+        distributions. The mean of the normal distribution is the gamma parameter and the variance is sampled from
+        an inverse gamma distribution and divided by the nu parameter. The first dimension is the mean and
+        the second dimension is the variance.
+
+        Args:
+            x: torch.Tensor, input data
+            n_samples: int, number of samples
+        Returns:
+            torch.Tensor, samples
+
+        """
+        x = self.model(x)
+        inverse_gamma = torch.distributions.InverseGamma(x["alpha"], x["beta"])
+        sigma2 = torch.stack([inverse_gamma.sample() for _ in range(n_samples)]).swapaxes(0, 1)
+        normal_mu = x["gamma"].unsqueeze(-1).expand(-1, n_samples, 1)
+        normal_sigma2 = sigma2 / x["nu"].unsqueeze(2)
+        x = torch.cat((normal_mu, normal_sigma2), dim=2)
+        return x
