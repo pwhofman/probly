9411d9ebdf35cbaafd1324ee6a759e7d0be58979
Paul Hofman <paulwhofman@hotmail.com>
2025-04-14 22:17:25 +0200

    Make sure submodules get imported when importing probly

--- src/probly/__init__.py
+++ src/probly/__init__.py
@@ -1,1 +1,13 @@
 """probly: Uncertainty Representation and Quantification for Machine Learning."""
+
+from probly import (
+    calibration as calibration,
+    data as data,
+    losses as losses,
+    metrics as metrics,
+    plot as plot,
+    quantification as quantification,
+    representation as representation,
+    tasks as tasks,
+    utils as utils,
+)
