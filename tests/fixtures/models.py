d211093c833e4a2a7a494dc5a63c40e50bed9b28
Paul Hofman <paulwhofman@hotmail.com>
2025-04-25 15:15:38 +0200

    Add tests for losses

--- tests/fixtures/models.py
+++ tests/fixtures/models.py
@@ -22,3 +22,15 @@ def model_small_2d_2d() -> torch.nn.Module:
 def conv_linear_model() -> nn.Module:
     model = nn.Sequential(nn.Conv2d(3, 5, 5), nn.ReLU(), nn.Flatten(), nn.Linear(5, 2))
     return model
+
+
+@pytest.fixture
+def regression_model_1d() -> nn.Module:
+    model = nn.Sequential(nn.Linear(2, 2), nn.ReLU(), nn.Linear(2, 1))
+    return model
+
+
+@pytest.fixture
+def regression_model_2d() -> nn.Module:
+    model = nn.Sequential(nn.Linear(4, 4), nn.ReLU(), nn.Linear(4, 2))
+    return model
