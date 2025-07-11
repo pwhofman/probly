d1108afc867d48079265667e882e5c80185fc196
Maximilian <maximilian.muschalik@gmail.com>
2025-07-11 17:46:46 +0200

    Adds Deprecation Test Suite and Refactors Utils into a package. (#118)
    * adds deprecation test module to tests
    
    * adds pytest-randomly to test suite
    
    * adds pytest-randomly to test suite

--- src/probly/utils/probabilities.py
+++ src/probly/utils/probabilities.py
@@ -2,63 +2,9 @@
 
 from __future__ import annotations
 
-import itertools
-from typing import TYPE_CHECKING
-
 import numpy as np
 import torch
 import torch.nn.functional as F
-from tqdm import tqdm
-
-if TYPE_CHECKING:
-    from collections.abc import Iterable
-
-
-def powerset(iterable: Iterable[int]) -> list[tuple[()]]:
-    """Generate the power set of a given iterable.
-
-    Args:
-        iterable: Iterable
-    Returns:
-        List[tuple], power set of the given iterable
-
-    """
-    s = list(iterable)
-    return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s) + 1)))
-
-
-def capacity(q: np.ndarray, a: Iterable[int]) -> np.ndarray:
-    """Compute the capacity of set q given set a.
-
-    Args:
-        q: numpy.ndarray, shape (n_instances, n_samples, n_classes)
-        a: Iterable, shape (n_classes,), indices indicating subset of classes
-    Returns:
-        min_capacity: numpy.ndarray, shape (n_instances,), capacity of q given a
-
-    """
-    selected_sum = np.sum(q[:, :, a], axis=2)
-    min_capacity = np.min(selected_sum, axis=1)
-    return min_capacity
-
-
-def moebius(q: np.ndarray, a: Iterable[int]) -> np.ndarray:
-    """Compute the Moebius function of a set q given a set a.
-
-    Args:
-        q: numpy.ndarray of shape (num_samples, num_members, num_classes)
-        a: numpy.ndarray, shape (n_classes,), indices indicating subset of classes
-    Returns:
-        m_a: numpy.ndarray, shape (n_instances,), moebius value of q given a
-
-    """
-    ps_a = powerset(a)  # powerset of A
-    ps_a.pop(0)  # remove empty set
-    m_a = np.zeros(q.shape[0])
-    for b in ps_a:
-        dl = len(set(a) - set(b))
-        m_a += ((-1) ** dl) * capacity(q, b)
-    return m_a
 
 
 def differential_entropy_gaussian(sigma2: float | np.ndarray, base: float = 2) -> float | np.ndarray:
@@ -95,44 +41,6 @@ def kl_divergence_gaussian(
     """
     kl_div = 0.5 * np.log(sigma22 / sigma21) / np.log(base) + (sigma21 + (mu1 - mu2) ** 2) / (2 * sigma22) - 0.5
     return kl_div
-
-
-def torch_reset_all_parameters(module: torch.nn.Module) -> None:
-    """Reset all parameters of a torch module.
-
-    Args:
-        module: torch.nn.Module, module to reset parameters
-
-    """
-    if hasattr(module, "reset_parameters"):
-        module.reset_parameters()
-    for child in module.children():
-        if hasattr(child, "reset_parameters"):
-            child.reset_parameters()
-
-
-@torch.no_grad()
-def torch_collect_outputs(
-    model: torch.nn.Module,
-    loader: torch.utils.data.DataLoader,
-    device: torch.device,
-) -> tuple[torch.Tensor, torch.Tensor]:
-    """Collect outputs and targets from a model for a given data loader.
-
-    Args:
-        model: torch.nn.Module, model to collect outputs from
-        loader: torch.utils.data.DataLoader, data loader to collect outputs from
-        device: torch.device, device to move data to
-    Returns:
-        outputs: torch.Tensor, shape (n_instances, n_classes), model outputs
-        targets: torch.Tensor, shape (n_instances,), target labels
-    """
-    outputs = torch.empty(0, device=device)
-    targets = torch.empty(0, device=device)
-    for inpt, target in tqdm(loader):
-        outputs = torch.cat((outputs, model(inpt.to(device))), dim=0)
-        targets = torch.cat((targets, target.to(device)), dim=0)
-    return outputs, targets
 
 
 def temperature_softmax(logits: torch.Tensor, temperature: float | torch.Tensor) -> torch.Tensor:
