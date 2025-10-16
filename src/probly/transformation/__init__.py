aa840f26557437e2453aecc4d20898d3efb64b2b
Paul Hofman <paulwhofman@hotmail.com>
2025-10-16 16:01:23 +0200

    Implement Bayesian transformation

--- src/probly/transformation/__init__.py
+++ src/probly/transformation/__init__.py
@@ -1,6 +1,7 @@
 """Transformations for models."""
 
+from probly.transformation.bayesian import bayesian
 from probly.transformation.dropconnect import dropconnect
 from probly.transformation.dropout import dropout
 
-__all__ = ["dropconnect", "dropout"]
+__all__ = ["bayesian", "dropconnect", "dropout"]
--- src/probly/transformation/layers/__init__.py
+++ src/probly/transformation/layers/__init__.py
@@ -1,5 +1,6 @@
 """Init module for layer implementations."""
 
+from probly.transformation.layers.bayesian import BayesConv2d, BayesLinear
 from probly.transformation.layers.dropconnect import DropConnectLinear
 
-__all__ = ["DropConnectLinear"]
+__all__ = ["BayesConv2d", "BayesLinear", "DropConnectLinear"]
