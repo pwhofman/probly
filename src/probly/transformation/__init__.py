6877bf0548a54371685888a0f8a6abba54a4fcf8
Timo <t.loehr@lmu.de>
2025-10-20 16:22:53 +0200

    Start implementation of ensemble for multi library support

--- src/probly/transformation/__init__.py
+++ src/probly/transformation/__init__.py
@@ -3,5 +3,6 @@
 from probly.transformation.bayesian import bayesian
 from probly.transformation.dropconnect import dropconnect
 from probly.transformation.dropout import dropout
+from probly.transformation.ensemble import ensemble
 
-__all__ = ["bayesian", "dropconnect", "dropout"]
+__all__ = ["bayesian", "dropconnect", "dropout", "ensemble"]
