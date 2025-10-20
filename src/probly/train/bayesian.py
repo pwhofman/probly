4af65a2efc59d0ea011132adfed12349e2cdfe88
Paul Hofman <paulwhofman@hotmail.com>
2025-10-20 13:24:56 +0200

    Improve typing bayesian.py

--- src/probly/train/bayesian.py
+++ src/probly/train/bayesian.py
@@ -5,12 +5,11 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from torch import nn
-
-    from probly.transformation.layers.torch import BayesConv2d, BayesLinear
+    from probly.predictor import Predictor
 
 import torch
 
+from probly.transformation.layers.torch import BayesConv2d, BayesLinear  # noqa: TC001, required by traverser
 from probly.traverse_nn import nn_compose
 from pytraverse import GlobalVariable, State, TraverserResult, singledispatch_traverser, traverse_with_state
 
@@ -27,7 +26,7 @@ def kl_divergence_traverser(
     return obj, state
 
 
-def collect_kl_divergence(model: nn.Module) -> torch.Tensor | float:
+def collect_kl_divergence(model: Predictor) -> torch.Tensor | float:
     """Collect the KL divergence of the Bayesian model by summing the KL divergence of each Bayesian layer."""
     _, state = traverse_with_state(model, nn_compose(kl_divergence_traverser))
     return state[KL_DIVERGENCE]
