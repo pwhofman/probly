c45c94a86c5e75fd0891eaa9557bf09f963afbbe
Paul Hofman <paulwhofman@hotmail.com>
2025-10-21 15:14:05 +0200

    Further structure of tests

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-"""Test fixtures for the package."""
-
-from __future__ import annotations
-
-pytest_plugins = [
-    "tests.fixtures.models",
-]
---
+++ tests/probly/conftest.py
@@ -0,0 +1,7 @@
+"""Test fixtures for probly."""
+
+from __future__ import annotations
+
+pytest_plugins = [
+    "tests.probly.fixtures.models",
+]
