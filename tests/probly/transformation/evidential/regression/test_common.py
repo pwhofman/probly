e73239ede4601b43de59e6cd57a67c80565b09e7
Julia Goihman <87q5h78tj4@privaterelay.appleid.com>
2025-10-27 11:43:45 +0100

    Update test_common.py

--- tests/probly/transformation/evidential/regression/test_common.py
+++ tests/probly/transformation/evidential/regression/test_common.py
@@ -1,92 +1,78 @@
-import sys
-import types
 import importlib
 import pytest
 
-class FakeGlobalVariable:
-    def __init__(self, name, description, default=False):
-        self.name = name
-        self.description = description
-        self.default = default
-    def __class_getitem__(cls, item):
-        return cls
 
-class FakeTraverser:
-    def __init__(self, name):
-        self.name = name
-        self.register_calls = []
-    def register(self, **kwargs):
-        self.register_calls.append(kwargs)
+def test_register_attaches_skip_if_and_uses_global_flag(mocker):
+    import probly.transformation.evidential.regression.common as common
+    importlib.reload(common)
 
-class FakeLazyDispatch:
-    def __getitem__(self, item):
-        def creator(name):
-            return FakeTraverser(name=name)
-        return creator
+    spy = mocker.spy(common.evidential_regression_traverser, "register")
 
-def install_fakes():
-    probly = types.ModuleType("probly")
-    traverse_nn = types.ModuleType("probly.traverse_nn")
-    pytraverse = types.ModuleType("pytraverse")
+    class DummyCls: ...
+    dummy_traverser = object()
 
-    def nn_compose(x):
-        return ("COMPOSED", x)
-    traverse_nn.nn_compose = nn_compose
+    common.register(DummyCls, dummy_traverser)
 
-    CLONE = object()
-    TRAVERSE_REVERSED = object()
+    spy.assert_called_once()
+    kwargs = spy.call_args.kwargs
+    assert kwargs["cls"] is DummyCls
+    assert kwargs["traverser"] is dummy_traverser
 
-    def traverse(base, composed, init):
-        traverse.last_call = (base, composed, init)
-        return "TRANSFORMED"
+    skip_if = kwargs["skip_if"]
+    flag = common.REPLACED_LAST_LINEAR
+    assert skip_if({flag: True}) is True
+    assert skip_if({flag: False}) is False
+    try:
+        res = skip_if({})
+    except KeyError:
+        res = None
+    assert res in (None, False)
 
-    pytraverse.CLONE = CLONE
-    pytraverse.TRAVERSE_REVERSED = TRAVERSE_REVERSED
-    pytraverse.GlobalVariable = FakeGlobalVariable
-    pytraverse.lazydispatch_traverser = FakeLazyDispatch()
-    pytraverse.traverse = traverse
 
-    sys.modules["probly"] = probly
-    sys.modules["probly.traverse_nn"] = traverse_nn
-    sys.modules["pytraverse"] = pytraverse
+def test_evidential_regression_calls_traverse_with_expected_init_and_compose(mocker):
+    import probly.transformation.evidential.regression.common as common
+    import pytraverse
+    importlib.reload(common)
+    importlib.reload(pytraverse)
 
-@pytest.fixture(autouse=True)
-def clean_imports():
-    victims = [k for k in list(sys.modules) if k.startswith("probly.transformation.evidential.regression.common")]
-    for k in victims:
-        sys.modules.pop(k, None)
-    yield
-    for k in victims:
-        sys.modules.pop(k, None)
+    spy = mocker.spy(pytraverse, "traverse")
 
-def test_register_and_skip_if_behaviour():
-    install_fakes()
-    mod = importlib.import_module("probly.transformation.evidential.regression.common")
-    assert isinstance(mod.REPLACED_LAST_LINEAR, FakeGlobalVariable)
-    assert mod.REPLACED_LAST_LINEAR.default is False
-    assert mod.evidential_regression_traverser.name == "evidential_regression_traverser"
-    dummy_cls = object()
-    dummy_trav = object()
-    mod.register(dummy_cls, dummy_trav)
-    assert mod.evidential_regression_traverser.register_calls
-    call = mod.evidential_regression_traverser.register_calls[-1]
-    assert call["cls"] is dummy_cls
-    assert call["traverser"] is dummy_trav
-    skip_if = call["skip_if"]
-    assert skip_if({mod.REPLACED_LAST_LINEAR: True}) is True
-    assert skip_if({mod.REPLACED_LAST_LINEAR: False}) is False
+    class BasePredictor:
+        pass
 
-def test_evidential_regression_traverse_called_and_returns_transformed():
-    install_fakes()
-    mod = importlib.import_module("probly.transformation.evidential.regression.common")
-    base = object()
-    result = mod.evidential_regression(base)
-    assert result == "TRANSFORMED"
-    base_arg, composed_arg, init_arg = sys.modules["pytraverse"].traverse.last_call
+    base = BasePredictor()
+
+    result = common.evidential_regression(base)
+
+    assert spy.call_count >= 1
+    call_args, call_kwargs = spy.call_args
+    base_arg, composed_arg, init_arg = call_args[:3]
+
     assert base_arg is base
-    tag, traverser = composed_arg
-    assert tag == "COMPOSED"
-    assert isinstance(traverser, FakeTraverser)
-    assert traverser.name == "evidential_regression_traverser"
-    assert init_arg[sys.modules["pytraverse"].TRAVERSE_REVERSED] is True
-    assert init_arg[sys.modules["pytraverse"].CLONE] is True
+    expected_composed = common.nn_compose(common.evidential_regression_traverser)
+    assert composed_arg == expected_composed
+    assert init_arg.get(pytraverse.TRAVERSE_REVERSED) is True
+    assert init_arg.get(pytraverse.CLONE) is True
+    assert result is not None or result is None
+
+
+@pytest.mark.parametrize("flag_value", [True, False])
+def test_skip_logic_via_flag_end_to_end(flag_value):
+    import probly.transformation.evidential.regression.common as common
+    importlib.reload(common)
+
+    captured = {}
+
+    def _capture_register(**kwargs):
+        captured["skip_if"] = kwargs["skip_if"]
+
+    original_register = common.evidential_regression_traverser.register
+    try:
+        common.evidential_regression_traverser.register = _capture_register
+        common.register(object, object())
+    finally:
+        common.evidential_regression_traverser.register = original_register
+
+    skip_if = captured["skip_if"]
+    flag = common.REPLACED_LAST_LINEAR
+    assert skip_if({flag: flag_value}) is flag_value
