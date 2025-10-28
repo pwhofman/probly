8f56cbf77defa6deb2fe81a2fed5df0774d86cad
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2025-10-28 22:20:29 +0100

    Implement DropConnectLinear wrapper for Flax nnx.Linear

---
+++ src/probly/transformation/dropconnect/flax.py
@@ -0,0 +1,15 @@
+"""Flax dropconnect implementation."""
+
+from __future__ import annotations
+
+from flax import nnx
+
+from probly.layers.flax import DropConnectLinear
+
+from .common import register
+
+def replace_flax_dropconnect(obj: nnx.Linear, p: float) -> DropConnectLinear:
+    """Replace a given layer by a DropConnectLinear layer."""
+    return DropConnectLinear(obj, p=p)
+
+register(nnx.Linear, replace_flax_dropconnect)
