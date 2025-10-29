c8f6480eba16b4501e52b7bb3ffed52f2c682c87
Elias Barth <160789026+elias0224@users.noreply.github.com>
2025-10-29 19:33:35 +0100

    change from random params to same params

--- src/probly/transformation/ensemble/flax.py
+++ src/probly/transformation/ensemble/flax.py
@@ -10,19 +10,16 @@ import jax.numpy as jnp
 
 from .common import register 
 
-def _reset_clone(obj: nnx.Module, input_shape: tuple, key: jax.random.PRNGKey) -> Tuple[nnx.Module, dict]:
+def _reset_clone(obj: nnx.Module) -> Tuple[nnx.Module, dict]:
     """New params initialized.
     
     Args:
         obj: nnx.Module, The model to be cloned.
-        input_shape: tuple, The shape of the input data.
-        key: jax.random.PRNGKey, The random key for initialization.
         
     Returns:
         Tuple[nnx.Module, dict], The cloned model and its parameters.
     """
-    x = jnp.ones(input_shape)
-    params = obj.init(key, x)
+    params = nnx.clone(obj) 
     return obj, params
 
 def generate_flax_ensemble(obj: nnx.Module, n_members: int, input_shape: tuple, key: jax.random.PRNGKey) -> List[Tuple[nnx.Module, dict]]:
@@ -31,13 +28,11 @@ def generate_flax_ensemble(obj: nnx.Module, n_members: int, input_shape: tuple, 
     Args:
         obj: nnx.Module, The base model to be used for the ensemble.
         n_members: int, The number of members in the ensemble.
-        input_shape: tuple, The shape of the input data.
-        key: jax.random.PRNGKey, The random key for initialization.
+        
     
     Returns:
         List[Tuple[nnx.Module, dict]], The list of ensemble members with their parameters.
     """
-    subkeys = jax.random.split(key, n_members)
-    return [_reset_clone(obj, input_shape, k) for k in subkeys]
+    return [_reset_clone(obj) for _ in range(n_members)]
    
 register(nnx.Module, generate_flax_ensemble)
