90efa37eb64d8baa19d22a77489504d6a981fb54
kiksknacks <katrin.stecher@campus.lmu.de>
2025-10-29 22:39:35 +0100

    minor changes to function

--- tests/probly/transformation/ensemble/test_common.py
+++ tests/probly/transformation/ensemble/test_common.py
@@ -6,8 +6,8 @@ import pytest
 
 from probly.predictor import Predictor
 from probly.transformation import ensemble
-
-def test_ensemble_generator_when_not_registered(self, dummy_predictor: Predictor) -> None:
-    n_members=3
-    with pytest.raises(NotImplementedError, match=f"No ensemble generator is registered for type {type(dummy_predictor)}"):
-        ensemble(base, n_members=n_members)
+       
+def test_ensemble_generator_when_not_registered(dummy_predictor) -> None:
+    n_members = 3
+    with pytest.raises(NotImplementedError, match=f"No ensemble generator is registered for type",):
+        ensemble(dummy_predictor, n_members=n_members)
