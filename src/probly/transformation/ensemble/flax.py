e8e40981ed6893e850d5f7f4b0f05a5285f6adea
Celina <celina@surrer.com>
2025-10-29 21:42:27 +0100

    "Flax_Test"

--- src/probly/transformation/ensemble/flax.py
+++ src/probly/transformation/ensemble/flax.py
@@ -1,9 +1,12 @@
 """Flax ensemble implementation."""
 from __future__ import annotations
+
 from flax.nnx import Module, Rngs
 import jax
+
 from probly.traverse_nn import nn_compose
 from pytraverse import CLONE, lazydispatch_traverser, traverse
+
 from .common import register
 
 reset_traverser_nnx = lazydispatch_traverser[object](name="reset_traverser_nnx")
