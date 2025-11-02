fc25990dad96ea0cc0612e04310c4b9efa7807be
m3ryemmo <Meryemmohamed61@gmail.com>
2025-11-02 12:37:19 +0100

    fixed code, removed flax test

--- tests/probly/transformation/bayesian/test_torch.py
+++ tests/probly/transformation/bayesian/test_torch.py
@@ -10,7 +10,7 @@ from probly.transformation import bayesian
 torch = pytest.importorskip("torch")
 
 
-def tests_bayesian_noo_param_torch(torch_model_small_2d_2d: nn.Module) -> None:
+def tests_bayesian_noo_param_torchh(torch_model_small_2d_2d: nn.Module) -> None:
     """Test that bayesian can be called with a torch model without prior or likelihood."""
     result = bayesian(torch_model_small_2d_2d)
 
