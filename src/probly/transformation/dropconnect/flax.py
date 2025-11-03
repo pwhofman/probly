5ba165afc5d4bea0c6dcf63f3ff873324d7d0a69
zelihabeyza <zeliha.beyza.ce@gmail.com>
2025-11-03 14:36:45 +0100

    dropconnect tests (fix??)

--- src/probly/transformation/dropconnect/flax.py
+++ src/probly/transformation/dropconnect/flax.py
@@ -2,18 +2,16 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from flax import nnx
+
+from probly.layers.torch import DropConnectLinear
 
 from .common import register
 
-if TYPE_CHECKING:
-    from probly.layers.torch import DropConnectLinear
 
-    def replace_flax_dropconnect(obj: nnx.Linear, p: float) -> DropConnectLinear:
-        """Replace a given nnx.layer by a DropConnectLinear layer."""
-        return DropConnectLinear(obj, p=p)
+def replace_flax_dropconnect(obj: nnx.Linear, p: float) -> DropConnectLinear:
+    """Replace a given nnx.layer by a DropConnectLinear layer."""
+    return DropConnectLinear(obj, p=p)
 
 
 register(nnx.Linear, replace_flax_dropconnect)
