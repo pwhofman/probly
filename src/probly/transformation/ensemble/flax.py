5510203134a4f927825250b3a9452169a0c1de59
Nadia Fae Cadacio <nadiacadacio@hotmail.com>
2025-11-03 23:20:12 +0100

    Ensemble for flax

--- src/probly/transformation/ensemble/flax.py
+++ src/probly/transformation/ensemble/flax.py
@@ -12,9 +12,7 @@ if TYPE_CHECKING:
     from collections.abc import Callable
 
 def generate_flax_ensemble(obj: nnx.Module, n_members: int) -> list[nnx.Module]:
-    base_model = obj.__class__
-    rngs = nnx.Rngs(params=0)
-    my_model = base_model(rngs=rngs)
+    base_model = obj.__class__ 
     models = []
 
     for x in range(n_members):
---
+++ tests/probly/transformation/ensemble/test_flax.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import pytest
+
+from probly.transformation.ensemble.flax import generate_flax_ensemble
+
+from flax import nnx
