e4204aba9637fbde500ad18448ed94716919bdf8
Paul Hofman <paulwhofman@hotmail.com>
2025-11-04 12:53:15 +0100

    Restructure tests to match src/probly train module

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,6 @@ from __future__ import annotations
 pytest_plugins = [
     "tests.probly.fixtures.common",
     "tests.probly.fixtures.torch_models",
+    "tests.probly.fixtures.torch_data",
     "tests.probly.fixtures.flax_models",
 ]
