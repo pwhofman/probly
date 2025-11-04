9f6c2ffb4f170108be1f860a1da9a9ef9ed5daee
Elias Barth <160789026+elias0224@users.noreply.github.com>
2025-11-04 18:21:52 +0100

    changed flax support for ensemble after testing
    # Conflicts:
    #	src/probly/transformation/ensemble/flax.py
    #	tests/probly/transformation/ensemble/__init__.py

--- src/probly/transformation/ensemble/flax.py
+++ src/probly/transformation/ensemble/flax.py
@@ -4,27 +4,27 @@ from __future__ import annotations
 
 from flax import nnx
 import jax
-import jax.numpy as jnp
 
 from .common import register
 
 
-def _clone(obj: nnx.Module) -> tuple[nnx.Module, dict]:
+def _clone(obj: nnx.Module) -> nnx.Module:
     """Deep copy of params for flax module."""
-    params = nnx.clone(obj)
-    return obj, params
+    cloned_model = nnx.clone(obj)
+    return cloned_model
 
 
-def _random_reset(obj: nnx.Module, key: jax.random.PRNGKey, shape: jnp.ndarray) -> tuple[nnx.Module, dict]:
-    """Reset params for flax module."""
-    params = nnx.init(key, shape)
-    return obj, params
+def _clone_reset(obj: nnx.Module, rng: jax.random.PRNGKey) -> nnx.Module:
+    """Deep copy of params for flax module with re-initialization."""
+    cloned_model = nnx.clone(obj)
+    nnx.reseed(cloned_model, policy="match_shape", rng=rng)
+    return cloned_model
 
 
-def generate_flax_ensemble(obj: nnx.Module, num_members: int, reset_params: bool) -> list[tuple[nnx.Module, dict]]:
+def generate_flax_ensemble(obj: nnx.Module, num_members: int, reset_params: bool) -> list[nnx.Module]:
     """Build a flax ensemble by initializing n_members times."""
     if reset_params:
-        return [_random_reset(obj, jax.random.PRNGKey(i), jnp.ones((1,))) for i in range(num_members)]
+        return [_clone_reset(obj, jax.random.PRNGKey(i)) for i in range(num_members)]
     return [_clone(obj) for _ in range(num_members)]
 
 
