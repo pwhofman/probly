92be7bd70f1c833d775b25cf13990eee1497d8fd
Nadia Fae Cadacio <nadiacadacio@hotmail.com>
2025-11-05 16:37:58 +0100

    Ensemble implementation in flax

--- src/probly/transformation/ensemble/flax.py
+++ src/probly/transformation/ensemble/flax.py
@@ -3,19 +3,23 @@
 from __future__ import annotations
 
 from flax import nnx
+import jax
 
 from .common import register
 
 
-def generate_flax_ensemble(obj: nnx.Module, n_members: int) -> list[nnx.Module]:  # noqa: D103
-    base_model = obj.__class__
+def generate_flax_ensemble(obj: nnx.Module, n_members: int) -> list[nnx.Module]:
+    base_model = obj.__class__ 
     models = []
-
-    for x in range(n_members):
+    # Creates a base random key
+    rng = jax.random.key(0)
+    # Split the base key into n independent RNG subkeys ensuring each model has a different key
+    rngs_list = jax.random.split(rng, n_members)
+    for x in rngs_list:
+        # Initialize a new model with its own random parameters
         rngs = nnx.Rngs(params=x)
-        new_model = base_model(rngs=rngs)
+        new_model = type(obj)(rngs=rngs)
         models.append(new_model)
-
     return models
 
 
