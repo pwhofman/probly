688e955acb11d0ea9cb20e9f9f0be4d13ebc8d9f
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2025-11-05 20:34:18 +0100

    Fix double counting

--- src/probly/transformation/dropconnect/flax.py
+++ src/probly/transformation/dropconnect/flax.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import contextlib
+
 from flax import nnx
 
 from probly.layers.flax import DropConnectLinear
@@ -11,7 +13,14 @@ from .common import register
 
 def replace_flax_dropconnect(obj: nnx.Linear, p: float) -> DropConnectLinear:
     """Replace a given layer by a DropConnectLinear layer."""
-    return DropConnectLinear(obj, p=p)
+    dc_layer = DropConnectLinear(obj, p=p)
+
+    # Remove the reference to the old base layer
+    if hasattr(dc_layer, "base_layer"):
+        with contextlib.suppress(Exception):
+            delattr(dc_layer, "base_layer")
+
+    return dc_layer
 
 
 register(nnx.Linear, replace_flax_dropconnect)
