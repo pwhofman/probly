c667233199ea3d37d3c0d3d222ebdd9308c31f5a
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2025-11-11 14:16:01 +0100

    Remove deletion of base_layer

--- src/probly/transformation/dropconnect/flax.py
+++ src/probly/transformation/dropconnect/flax.py
@@ -2,8 +2,6 @@
 
 from __future__ import annotations
 
-import contextlib
-
 from flax import nnx
 
 from probly.layers.flax import DropConnectLinear
@@ -13,14 +11,7 @@ from .common import register
 
 def replace_flax_dropconnect(obj: nnx.Linear, p: float) -> DropConnectLinear:
     """Replace a given layer by a DropConnectLinear layer."""
-    dc_layer = DropConnectLinear(obj, p=p)
-
-    # Remove the reference to the old base layer
-    if hasattr(dc_layer, "base_layer"):
-        with contextlib.suppress(Exception):
-            delattr(dc_layer, "base_layer")
-
-    return dc_layer
+    return DropConnectLinear(obj, rate=p)
 
 
 register(nnx.Linear, replace_flax_dropconnect)
