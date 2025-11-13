439b5755410bb4a3245f2b314db993390a303c3a
Shoha <shoha28kyu@gmail.com>
2025-11-13 17:19:00 +0100

    Modified tests to comply with Ruff, as well as added an extra tests to test_torch for convolutional linear models

--- tests/probly/transformation/evidential/classification/test_common.py
+++ tests/probly/transformation/evidential/classification/test_common.py
@@ -2,16 +2,15 @@
 
 from __future__ import annotations
 
-
 import pytest
 
 from probly.predictor import Predictor
-from probly.transformation.evidential.classification import evidential_classification 
+from probly.transformation.evidential.classification import evidential_classification
+
 
 def test_invalid_input_in_evidential_classification(dummy_predictor: Predictor) -> None:
-
-    """Tests the behavior of the evidential classification function when provided with a model that has no registered appender.
-
-    """
-    with pytest.raises(NotImplementedError, match=f"No evidential classification appender registered for type {type(dummy_predictor)}"):
+    """Tests the evidential classification function when provided with a model that has no registered appender."""
+    with pytest.raises(
+            NotImplementedError,
+            match=f"No evidential classification appender registered for type {type(dummy_predictor)}"):
         evidential_classification(dummy_predictor)
