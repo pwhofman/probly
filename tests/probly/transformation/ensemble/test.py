5f531a06310bb585d1bb4e843cfede14d0509f46
Davin <davindewanto@gmail.com>
2025-11-19 19:40:01 +0100

    Fixed issues and updated some features

---
+++ tests/probly/transformation/ensemble/test.py
@@ -0,0 +1,14 @@
+#Eingabe von Werten durch den Benutzer
+#input()
+name = input("Gib deinen Namen ein: ")
+print(name)
+
+#Datentype : String, Integer, Float(zB: 4.87), Boolean
+#Die Funktion input() gibt uns einen Wert vom Typ string zurueck
+print(type(name))
+
+alter = int(input("Gib dein Alter ein: "))
+print(type(alter))
+
+
+
