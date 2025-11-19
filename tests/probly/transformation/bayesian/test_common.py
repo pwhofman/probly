bc6d00a645239487967875a8193121c72ccdb40e
Michael R <mr@MacBook-Air-von-Michael.local>
2025-11-19 19:29:06 +0100

    Bayesian Changes

--- tests/probly/transformation/bayesian/test_common.py
+++ tests/probly/transformation/bayesian/test_common.py
@@ -1,27 +1,25 @@
-""" Test for the bayesian models. """
+"""Test for the bayesian models."""
 
 from __future__ import annotations
+
 import pytest
 
-from probly.predictor import Predictor
 from probly.transformation import bayesian
+from probly.predictor import Predictor
 
-from torch import nn
+
 
 
 
 class TestNetworkArchitectures:
 
 # Tests if a linear model can be replaced by bayesian() using dummy parameters
-    def test_replace_linear_model(self, torch_model_small_2d_2d: nn.Sequential) -> None:
-        model = bayesian(torch_model_small_2d_2d, True, 0.3, 1.5, 0.5)
+    def test_replace_linear_model(self, dummy_predictor: Predictor) -> None:
+        # Transform the dummy_predictor with dummy parameters
+        model = bayesian(dummy_predictor, True, 0.3, 1.5, 0.5)
 
+        # Check if there is a new model and if bayesian() returned the correct type, Predictor
         assert model is not None
-        assert isinstance(model, type(torch_model_small_2d_2d))
+        assert isinstance(model, Predictor)
 
-# Tests if a convolutional model can be replaced by bayesian() using dummy parameters
-    def test_replace_conv2d_model(self, torch_conv_linear_model: nn.Sequential) -> None:
-        model = bayesian(torch_conv_linear_model, True, 0.3, 1.5, 0.5)
 
-        assert model is not None
-        assert isinstance(model, type(torch_conv_linear_model))
--- tests/probly/transformation/bayesian/test_torch.py
+++ tests/probly/transformation/bayesian/test_torch.py
@@ -1,39 +1,34 @@
 from __future__ import annotations
 
 import pytest
+from torch import nn
 
 from probly.transformation.bayesian import torch
 from probly.layers.torch import BayesLinear, BayesConv2d
 from tests.probly.torch_utils import count_layers
 from probly.transformation import bayesian
 
-from torch import nn
-
 
 class TestNetworkArchitectures:
     """Test class for different network architectures."""
 
     def test_linear_network(self, torch_model_small_2d_2d: nn.Sequential) -> None:
-        """
-        This function verifies that:
-        - Every linear layer gets replaced by an bayesian layer.
-        - The structure of the model remains unchanged except for the added bayesian layers.
-        """
-        
-        model = bayesian(torch_model_small_2d_2d, True, 0.3, 1.5, 0.5)
+        """This function verifies that every linear layer gets replaced by an bayesian layer."""
+        """And that the structure of the model remains unchanged except for the added bayesian layers."""
+        model = bayesian(torch_model_small_2d_2d, False, 0.3, 1.5, 0.5)
 
-        # count number of nn.Linear layers in original model
+        # Count number of nn.Linear layers in original model
         count_linear_original = count_layers(torch_model_small_2d_2d, nn.Linear)
-        # count number of BayesLinear layers in original model
+        # Count number of BayesLinear layers in original model
         count_bayesian_original = count_layers(torch_model_small_2d_2d, BayesLinear)
-        # count number of nn.Sequential layers in original model
+        # Count number of nn.Sequential layers in original model
         count_sequential_original = count_layers(torch_model_small_2d_2d, nn.Sequential)
 
-        # count number of nn.Linear layers in modified model
+        # Count number of nn.Linear layers in modified model
         count_linear_modified = count_layers(model, nn.Linear)
-        # count number of BayesLinear layers in modified model
+        # Count number of BayesLinear layers in modified model
         count_bayesian_modified = count_layers(model, BayesLinear)
-        # count number of nn.Sequential layers in modified model
+        # Count number of nn.Sequential layers in modified model
         count_sequential_modified = count_layers(model, nn.Sequential)
 
         assert model is not None
@@ -45,28 +40,31 @@ class TestNetworkArchitectures:
 
 
 
+
     def test_convolutional_network(self, torch_conv_linear_model: nn.Sequential) -> None:
         """
         This function verifies that:
         - Every convolutional layer gets replaced by an bayesian layer.
         - The structure of the model remains unchanged except for the added bayesian layers.
         """
-        
+
         model = bayesian(torch_conv_linear_model, True, 0.3, 1.5, 0.5)
 
-        # count number of nn.Linear layers in original model
+        # Count number of nn.Linear layers in original model
         count_linear_original = count_layers(torch_conv_linear_model, nn.Linear)
-        # count number of nn.Dropout layers in original model
+        # Count number of nn.Dropout layers in original model
         count_bayesian_original = count_layers(torch_conv_linear_model, BayesConv2d)
-        # count number of nn.Sequential layers in original model
+        # Count number of nn.Sequential layers in original model
         count_sequential_original = count_layers(torch_conv_linear_model, nn.Sequential)
 
-        # count number of nn.Linear layers in modified model
+        # Count number of nn.Linear layers in modified model
         count_linear_modified = count_layers(model, nn.Linear)
-        # count number of nn.Dropout layers in modified model
+        # Count number of nn.Dropout layers in modified model
         count_bayesian_modified = count_layers(model, BayesConv2d)
-        # count number of nn.Sequential layers in modified model
+        # Count number of nn.Sequential layers in modified model
         count_sequential_modified = count_layers(model, nn.Sequential)
+
+
 
         assert model is not None
         assert isinstance(model, type(torch_conv_linear_model))
@@ -74,10 +72,3 @@ class TestNetworkArchitectures:
         assert count_linear_modified == 0
         assert count_bayesian_original == 0
         assert count_sequential_original == count_sequential_modified
-
-    
-
-
-    
-
-
