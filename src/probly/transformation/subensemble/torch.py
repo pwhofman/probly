f8c94108266f5565dc7d24753ae79811664bbd44
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2025-11-26 14:13:36 +0100

    Refactor to reuse ensemble\torch code and Add head model support and head_layer parameter

--- src/probly/transformation/subensemble/torch.py
+++ src/probly/transformation/subensemble/torch.py
@@ -4,44 +4,32 @@ from __future__ import annotations
 
 from torch import nn
 
-from probly.traverse_nn import nn_compose, nn_traverser
-from pytraverse import CLONE, singledispatch_traverser, traverse
+from probly.transformation.ensemble import ensemble
 
 from .common import register
-
-reset_traverser = singledispatch_traverser[nn.Module](name="reset_traverser")
-
-
-@reset_traverser.register
-def _(obj: nn.Module) -> nn.Module:
-    if hasattr(obj, "reset_parameters"):
-        obj.reset_parameters()  # type: ignore[operator]
-    return obj
-
-
-def _reset_copy(module: nn.Module) -> nn.Module:
-    return traverse(module, nn_compose(reset_traverser), init={CLONE: True})
-
-
-def _copy(module: nn.Module) -> nn.Module:
-    return traverse(module, nn_traverser, init={CLONE: True})
 
 
 def generate_torch_subensemble(
     obj: nn.Module,
     num_heads: int,
+    head: nn.Module | None = None,
     reset_params: bool = False,
+    head_layer: int = 1,
 ) -> nn.ModuleList:
-    """Build a torch subensemble by copying the last layer num_heads times, resetting the parameters of each head."""
-    feature_extractor = nn.Sequential(*list(obj.children())[:-1])
-    last_layer = list(obj.children())[-1]
+    """Build a torch subensemble by copying the last layer or head model num_heads times.
+
+    Resets the parameters of each head.
+    """
+    if head is None:
+        head = nn.Sequential(*list(obj.children())[-head_layer:])
+        obj = nn.Sequential(*list(obj.children())[:-head_layer])
 
     if reset_params:
-        heads = nn.ModuleList([_reset_copy(last_layer) for _ in range(num_heads)])
+        heads = ensemble(head, num_members=num_heads, reset_params=reset_params)
     else:
-        heads = nn.ModuleList([_copy(last_layer) for _ in range(num_heads)])
+        heads = ensemble(head, num_members=num_heads, reset_params=reset_params)
 
-    return nn.ModuleList([feature_extractor, heads])
+    return nn.ModuleList([obj, heads])
 
 
 register(nn.Module, generate_torch_subensemble)
