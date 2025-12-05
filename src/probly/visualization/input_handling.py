3bfeed92c6af56f138d54a4b1ed1628d41453448
Celina <celina@surrer.com>
2025-12-05 13:45:19 +0100

    partial input_handling and fixed docstrings in geometry (celiniebienie #314)

--- src/probly/visualization/input_handling.py
+++ src/probly/visualization/input_handling.py
@@ -1,1 +1,43 @@
 """Input Handling here."""
+
+from __future__ import annotations
+
+import numpy as np
+
+from .geometry import CredalVisualizer
+
+
+def check_num_classes(input_data: np.ndarray) -> None:
+    """Checks number of classes and refers to respective function.
+
+    Args:
+    input_data: array with last dimension equal to the number of classes.
+    """
+    viz = CredalVisualizer()
+    if input_data.shape[2] == 2:
+        return viz.interval_plot()  # not yet implemented
+    if input_data.shape[2] == 3:
+        return viz.ternary_plot(input_data)
+    if input_data.shape[2] == 4:
+        return viz.spider_plot()  # not yet implemented
+    raise NotImplementedError
+
+
+def check_shape(input_data: np.ndarray) -> None:
+    """Sanity check.
+
+    Args:
+    input_data: 3D tensor.
+    """
+    msg1 = "Input must be a tensor with shape (n_models, n_samples, n_classes."
+    msg2 = "There must be at least 2 classes."
+    msg3 = "The probabilities of each class must sum to 1."
+    msg4 = "All probabilities must be positive."
+    if not isinstance(input_data, np.ndarray) and input_data.ndim != 3:
+        raise ValueError(msg1)
+    if input_data.shape[2] <= 1:
+        raise ValueError(msg2)
+    if not np.allclose(input_data.sum(axis=-1), 1):
+        raise ValueError(msg3)
+    if (input_data < 0).any():
+        raise ValueError(msg4)
