ba096d9a5f81df0c7ef72a61e98b26c850ef2af7
Elias Barth <160789026+elias0224@users.noreply.github.com>
2025-12-08 15:27:34 +0100

    add different fixtures to testing

--- tests/probly/transformation/subensemble/test_flax.py
+++ tests/probly/transformation/subensemble/test_flax.py
@@ -16,17 +16,27 @@ from flax import nnx  # noqa: E402
 class TestGenerate:
     """Test class for subensemble generation."""
 
-    def test_number_of_heads(self, flax_model_small_2d_2d: nnx.Sequential) -> None:
+    @pytest.mark.parametrize(
+        "model_fixture",
+        [
+            "flax_model_small_2d_2d",
+            "flax_conv_linear_model",
+            "flax_regression_model_1d",
+            "flax_regression_model_2d",
+        ],
+    )
+    def test_number_of_heads(self, request, model_fixture) -> None:
         """Tests if the subensemble transformation creates the correct number of heads."""
         num_heads = 4
+        model = request.getfixturevalue(model_fixture)
 
         # default subensemble
-        subensemble_result = subensemble(flax_model_small_2d_2d, num_heads=num_heads, reset_params=True)
+        subensemble_result = subensemble(model, num_heads=num_heads, reset_params=True)
         backbone = subensemble_result[0]
         heads = subensemble_result[1]
 
-        original_layers = count_layers(flax_model_small_2d_2d, nnx.Linear)
-        backbone_layers = count_layers(backbone, nnx.Linear)
+        original_layers = count_layers(model, nnx.Module)
+        backbone_layers = count_layers(backbone, nnx.Module)
 
         # check tha backbone and heads are created correctly and heads have the correct structure
         assert isinstance(subensemble_result, nnx.Module)
@@ -37,15 +47,25 @@ class TestGenerate:
         for head in heads:
             assert count_layers(head, nnx.Linear) == 1
 
-    def test_number_of_heads_large_head_layer(self, flax_model_small_2d_2d: nnx.Sequential) -> None:
+    @pytest.mark.parametrize(
+        "model_fixture",
+        [
+            "flax_model_small_2d_2d",
+            "flax_conv_linear_model",
+            "flax_regression_model_1d",
+            "flax_regression_model_2d",
+        ],
+    )
+    def test_number_of_heads_large_head_layer(self, request, model_fixture) -> None:
         """Tests if the subensemble transformation creates the correct number and larger head_layer."""
         num_heads = 2
         # how many layers from the end should be considered as head
         head_layer = 2
+        model = request.getfixturevalue(model_fixture)
 
         # subensemble with input for head_layer
         subensemble_result = subensemble(
-            flax_model_small_2d_2d,
+            model,
             num_heads=num_heads,
             head_layer=head_layer,
             reset_params=True,
@@ -53,17 +73,25 @@ class TestGenerate:
         backbone = subensemble_result[0]
         heads = subensemble_result[1]
 
-        original_layers = count_layers(flax_model_small_2d_2d, nnx.Linear)
-        backbone_layers = count_layers(backbone, nnx.Linear)
+        original_layers = count_layers(model, nnx.Module)
+        backbone_layers = count_layers(backbone, nnx.Module)
 
         # check tha backbone is created correctly and heads have the correct structure
         assert isinstance(subensemble_result, nnx.Module)
         assert isinstance(backbone, nnx.Sequential)
         assert isinstance(heads, nnx.List)
-        assert original_layers - 2 == backbone_layers
+        if model_fixture == "flax_model_small_2d_2d":
+            assert original_layers - head_layer == backbone_layers
+        else:
+            # +1 because every model has only one "real" layer in the last two layers of the fixtures
+            assert original_layers - head_layer + 1 == backbone_layers
         assert len(heads) == num_heads
         for head in heads:
-            assert count_layers(head, nnx.Linear) == head_layer
+            # different check for different models because of differetn structures
+            if model_fixture == "flax_model_small_2d_2d":
+                assert count_layers(head, nnx.Module) - 2 == head_layer
+            else:
+                assert count_layers(head, nnx.Module) - 1 == head_layer
 
 
 class TestReset:
@@ -72,10 +100,11 @@ class TestReset:
     def test_parameters_reset(self, flax_model_small_2d_2d: nnx.Sequential) -> None:
         """Tests if the parameters of the model are reset correctly."""
         num_heads = 2
+        model = flax_model_small_2d_2d
 
-        original_params_last_layer = jax.tree_util.tree_leaves(flax_model_small_2d_2d.layers[-1])
+        original_params_last_layer = jax.tree_util.tree_leaves(model.layers[-1])
 
-        subensemble_result = subensemble(flax_model_small_2d_2d, num_heads=num_heads, reset_params=True)
+        subensemble_result = subensemble(model, num_heads=num_heads, reset_params=True)
         heads = subensemble_result[1]
 
         first_head = heads[0]
@@ -103,10 +132,11 @@ class TestReset:
     def test_parameters_not_reset(self, flax_model_small_2d_2d: nnx.Sequential) -> None:
         """Tests if the parameters of the model are not reset when specified."""
         num_heads = 2
+        model = flax_model_small_2d_2d
 
-        original_params_last_layer = jax.tree_util.tree_leaves(flax_model_small_2d_2d.layers[-1])
+        original_params_last_layer = jax.tree_util.tree_leaves(model.layers[-1])
 
-        subensemble_result = subensemble(flax_model_small_2d_2d, num_heads=num_heads, reset_params=False)
+        subensemble_result = subensemble(model, num_heads=num_heads, reset_params=False)
         heads = subensemble_result[1]
 
         for head in heads:
@@ -122,18 +152,16 @@ class TestReset:
         """Tests if the forward pass works correctly after subensemble transformation."""
         num_heads = 2
 
-        subensemble_result = subensemble(num_heads=num_heads, base=flax_model_small_2d_2d, reset_params=True)
+        subensemble_result = subensemble(flax_model_small_2d_2d, num_heads=num_heads, reset_params=True)
         backbone = subensemble_result[0]
         heads = subensemble_result[1]
 
-        # create dummy input
         x = jnp.ones((1, 2))
-
-        # forward pass through backbone
 
         backbone_output = backbone(x) if backbone is not None else x
 
         # forward pass through each head
         for head in heads:
             head_output = head(backbone_output)
-            assert head_output.shape == x.shape
+            expected_output_shape = (1, head.layers[-1].out_features)
+            assert head_output.shape == expected_output_shape
