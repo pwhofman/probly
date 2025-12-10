1207c006ae03b7b650b0bdce71e510178209eb4e
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2025-12-10 19:23:57 +0100

    Add Flax Support for DropConnect (#217)
    * Implement DropConnectLinear flax layer for Flax NNX
    
    * Implement DropConnectLinear wrapper for Flax nnx.Linear
    
    * Add Flax Support
    
    * Add docstring
    
    * Fix Ruff Errors
    
    * Fix Ruff format
    
    * Fix double counting
    
    * Added test for Flax implementation of Dropconnect
    
    * Fix linting issues
    
    * Fix linting issues
    
    * Fix linting issues
    
    * Fix linting issues
    
    * Fix linting issues
    
    * Fix linting issues
    
    * Remove deletion of base_layer
    
    * Refactor DropConnectLinear to align with Flax conventions
    
    * Change import location to pass checks
    
    * Refactor imports in test_flax.py
    
    Reorganize import statements for consistency.
    
    * Reorganize imports in test_flax.py
    
    Reordered import statements for consistency.
    
    ---------
    
    Co-authored-by: timkibele19-sys <timkibele19@gmail.com>
    Co-authored-by: Elias Barth <160789026+elias0224@users.noreply.github.com>
    Co-authored-by: Clemens Damke <c.damke@lmu.de>
    Group: ProBlym-Löser

--- src/probly/transformation/dropconnect/__init__.py
+++ src/probly/transformation/dropconnect/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from probly.lazy_types import TORCH_MODULE
+from probly.lazy_types import FLAX_MODULE, TORCH_MODULE
 
 from . import common
 
@@ -14,3 +14,9 @@ register = common.register
 @common.dropconnect_traverser.delayed_register(TORCH_MODULE)
 def _(_: type) -> None:
     from . import torch as torch  # noqa: PLC0415
+
+
+## Flax
+@common.dropconnect_traverser.delayed_register(FLAX_MODULE)
+def _(_: type) -> None:
+    from . import flax as flax  # noqa: PLC0415
