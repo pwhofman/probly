2183f3ae4998663993df86e5d618a9d4c14e8b99
Celina <celina@surrer.com>
2025-12-16 10:11:17 +0100

    Added show flag (celiniebienie #439)

--- src/probly/visualization/create_credal.py
+++ src/probly/visualization/create_credal.py
@@ -11,12 +11,17 @@ from probly.visualization.input_handling import dispatch_plot
 def create_credal_plot(
     input_data: np.ndarray,
     labels: list[str] | None = None,
-) -> None:
+    *,
+    show: bool = True,
+) -> plt.Axes | None:
     """One method for the user to call; Refers to correct plotting method via input_handling.
 
     Args:
     input_data: NumPy array with probabilities.
     labels: List of labels corresponding to the input data.
+    show: Enables the user to decide whether to show the plot or not.
     """
-    dispatch_plot(input_data, labels=labels)
-    plt.show()
+    plot = dispatch_plot(input_data, labels=labels)
+    if show:
+        plt.show()
+    return plot
