ca65132dc9ba2fd41fa51a1285e59870290fd7b2
Elias Barth <160789026+elias0224@users.noreply.github.com>
2026-01-13 19:46:15 +0100

    changed sklearn sample to have the same behavior as the array sample

--- src/probly/representation/sampling/sklearn_sample.py
+++ src/probly/representation/sampling/sklearn_sample.py
@@ -21,6 +21,9 @@ type Numeric = np.number | np.ndarray | float | int
 class SklearnSample(Sample[np.ndarray]):
     """A sample implementation for scikit-learn models."""
 
+    sample_axis: int
+    array: np.ndarray
+
     def from_iterable(cls, samples: Iterable[Numeric], sample_axis: SampleAxis = "auto", dtype: DTypeLike = None) -> Self:
         """Create an ArraySample for Sklearn from a sequence of samples.
 
@@ -55,25 +58,25 @@ class SklearnSample(Sample[np.ndarray]):
 
         return cls(array=samples, sample_axis=sample_axis)
     
-    def samples(cls, sample: Sample[Numeric], sample_axis: SampleAxis = "auto", dtype: DTypeLike = None) -> Iterable[np.ndarray]:
+    def samples(self) -> Iterable[np.ndarray]:
         """Return an iterator over the samples."""
-        if sample_axis == "auto":
-            sample_axis = 1
-        for i in range(sample.array.shape[sample_axis]):
-            yield np.take(sample.array, indices=i, axis=sample_axis)
+        if self.sample_axis == 0:
+            return self.array
+        return np.moveaxis(self.array, self.sample_axis, 0)
 
     def __init__(self, samples: list[np.ndarray], sample_axis: int = 1) -> None:
         """Initialize the sample."""
         self.array = np.stack(samples, axis=sample_axis)
+        self.sample_axis = sample_axis
 
-    def sample_mean(self, sample_axis: int = 1) -> np.ndarray:
+    def sample_mean(self) -> np.ndarray:
         """Compute mean across the samples axis."""
-        return self.array.mean(axis=sample_axis) 
-
-    def sample_std(self, ddof: int = 1, sample_axis: int = 1) -> np.ndarray:
+        return self.array.mean(axis=self.sample_axis) 
+    
+    def sample_std(self, ddof: int = 1) -> np.ndarray:
         """Compute standard deviation across the samples axis."""
-        return self.array.std(axis=sample_axis, ddof=ddof)
+        return self.array.std(axis=self.sample_axis, ddof=ddof)
 
-    def sample_var(self, ddof: int = 1, sample_axis: int = 1) -> np.ndarray:
+    def sample_var(self, ddof: int = 1) -> np.ndarray:
         """Compute variance across the samples axis."""
-        return self.array.var(axis=sample_axis, ddof=ddof) 
+        return self.array.var(axis=self.sample_axis, ddof=ddof) 
