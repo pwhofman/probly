bdeb3cb73e1533fab5235e0d3f71be8d179e59ee
Domi <238303207+dsme42@users.noreply.github.com>
2026-01-14 20:58:25 +0100

    Added conftest.py to isotonic regression tests

--- tests/probly/calibration/isotonic_regression/test_torch.py
+++ tests/probly/calibration/isotonic_regression/test_torch.py
@@ -2,45 +2,14 @@
 
 from __future__ import annotations
 
-import pytest
 from sklearn.isotonic import IsotonicRegression
 import torch
 from torch import Tensor, nn
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader
 
 from probly.calibration.isotonic_regression.torch import IsotonicRegressionCalibrator
 
 SetupReturnType = tuple[nn.Sequential, nn.Sequential, Tensor, DataLoader, DataLoader]
-
-
-@pytest.fixture
-def binary_torch_model() -> nn.Module:
-    return nn.Sequential(
-        nn.Linear(10, 2),
-    )
-
-
-@pytest.fixture
-def multiclass_torch_model() -> nn.Module:
-    return nn.Sequential(
-        nn.Linear(10, 4),
-    )
-
-
-@pytest.fixture
-def setup(binary_torch_model: nn.Sequential, multiclass_torch_model: nn.Sequential) -> SetupReturnType:
-    device = torch.device("cpu")
-    base_model_multiclass = multiclass_torch_model.to(device)
-    base_model_binary = binary_torch_model.to(device)
-
-    inputs = torch.randn(20, 10)
-    labels_multiclass = torch.randint(0, 3, (20,))
-    labels_binary = torch.randint(0, 2, (20,))
-
-    loader_multiclass = DataLoader(TensorDataset(inputs, labels_multiclass), batch_size=10)
-    loader_binary = DataLoader(TensorDataset(inputs, labels_binary), batch_size=10)
-
-    return base_model_multiclass, base_model_binary, inputs, loader_multiclass, loader_binary
 
 
 def test_fit_binary(setup: SetupReturnType) -> None:
