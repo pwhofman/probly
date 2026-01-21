c1491c7c22c086a875ade8f7d431b85105959011
zelihabeyza <zeliha.beyza.ce@gmail.com>
2026-01-21 22:30:21 +0100

    added cvplus.py and __init__.py fix

--- src/probly/conformal_prediction/methods/__init__.py
+++ src/probly/conformal_prediction/methods/__init__.py
@@ -1,6 +1,8 @@
 """Conformal Prediction methods implementation."""
 
 from probly.conformal_prediction.methods.common import ConformalClassifier, ConformalRegressor, Predictor, predict_probs
+from probly.conformal_prediction.methods.cvplus import CVPlusClassifier, CVPlusRegressor
+from probly.conformal_prediction.methods.jackknife import JackknifePlusClassifier, JackknifePlusRegressor
 from probly.conformal_prediction.methods.mondrian import (
     ClassConditionalClassifier,
     ClassConditionalRegressor,
@@ -11,11 +13,15 @@ from probly.conformal_prediction.methods.mondrian import (
 from probly.conformal_prediction.methods.split import SplitConformal, SplitConformalClassifier, SplitConformalRegressor
 
 __all__ = [
+    "CVPlusClassifier",
+    "CVPlusRegressor",
     "ClassConditionalClassifier",
     "ClassConditionalRegressor",
     "ConformalClassifier",
     "ConformalRegressor",
     "GroupedConformalBase",
+    "JackknifePlusClassifier",
+    "JackknifePlusRegressor",
     "MondrianConformalClassifier",
     "MondrianConformalRegressor",
     "Predictor",
---
+++ src/probly/conformal_prediction/methods/cvplus.py
@@ -0,0 +1,49 @@
+"""Cross-validation+ (CV+) implementation."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from probly.conformal_prediction.methods.common import Predictor
+    from probly.conformal_prediction.methods.jackknife import (
+        IntervalFunc,
+        ScoreFunc,
+    )
+
+from probly.conformal_prediction.methods.jackknife import (
+    JackknifePlusClassifier,
+    JackknifePlusRegressor,
+)
+
+
+class CVPlusRegressor(JackknifePlusRegressor):
+    """CV+ Regressor (Defaults to 5-Fold)."""
+
+    def __init__(
+        self,
+        model_factory: Callable[[], Predictor],
+        cv: int = 5,
+        random_state: int | None = None,
+        score_func: ScoreFunc | None = None,
+        interval_func: IntervalFunc | None = None,
+    ) -> None:
+        """Initialize CV+ Regressor."""
+        super().__init__(model_factory, cv, random_state, score_func, interval_func)
+
+
+class CVPlusClassifier(JackknifePlusClassifier):
+    """CV+ Classifier (Defaults to 5-Fold)."""
+
+    def __init__(
+        self,
+        model_factory: Callable[[], Predictor],
+        cv: int = 5,
+        random_state: int | None = None,
+        use_accretive: bool = False,
+        score_func: ScoreFunc | None = None,
+    ) -> None:
+        """Initialize CV+ Classifier."""
+        super().__init__(model_factory, cv, random_state, use_accretive, score_func)
