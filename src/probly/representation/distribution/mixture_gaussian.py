35288ec2db77bb794905567fa176af6600dc4a0b
Sonjaaaaa799 <sonjasofic@gmail.com>
2026-01-27 22:36:57 +0100

    added shape, ndim, size and dtype to mixture gaussian

--- src/probly/representation/distribution/mixture_gaussian.py
+++ src/probly/representation/distribution/mixture_gaussian.py
@@ -12,6 +12,8 @@ from probly.representation.sampling.array_sample import ArraySample
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
+    from numpy.typing import DTypeLike
+
     from probly.representation.distribution.array_gaussian import ArrayGaussian
 
 _RNG = np.random.default_rng()
@@ -68,3 +70,23 @@ class ArrayGaussianMixture:
             result[indices_for_component] = samples_for_component
 
         return ArraySample(array=result, sample_axis=0)
+
+    @property
+    def shape(self) -> tuple[int, ...]:
+        """Return the shape of the underlying component array."""
+        return self.components.shape
+
+    @property
+    def ndim(self) -> int:
+        """Return the number of dimensions of the component array."""
+        return self.components.ndim
+
+    @property
+    def size(self) -> int:
+        """Return the total number of elements in the component array."""
+        return self.components.size
+
+    @property
+    def dtype(self) -> DTypeLike:
+        """Return the data type of the underlying component array."""
+        return self.components.dtype
