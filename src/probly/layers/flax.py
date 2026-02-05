c710f605145cbe9a90ad29b8d000a3cf300e7174
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2026-02-05 13:56:08 +0100

    ProBlym-Löser: Flax prng/rngs handling (#271)
    * Add flax rngs handling
    
    * Add Notebook demonstrating prng/rngs handling
    
    * Add missing citation from n-te Gruppe
    
    * Refactor to mirror dropout\flax types
    
    * Refactor import/type signature
    
    * Refactor dropout transformation package to support rngs and rng_collection user input
    
    * Refactor dropconnect transformation package to support rngs and rng_collection user input
    
    * Refactor flax DropConnectLinear layer to apply a mask to weights during training, similar to layers/torch DropConnectLinear
    
    * Add Dropout and DropConnect flax fixtures
    
    * Refactor to use parametrized fixtures and Add tests for DropConnectLinear attributes and calls
    
    * Refactor to use parametrized fixtures
    
    * Refactor to explain rngs with streams, more basic
    
    * Add missing citation from n-te Gruppe
    
    * Fix ImportError and Add tests for rngs initialization and calls
    
    ---------
    
    Co-authored-by: Paul Hofman <paulwhofman@hotmail.com>

--- src/probly/layers/flax.py
+++ src/probly/layers/flax.py
@@ -6,22 +6,38 @@ from flax import nnx
 from flax.nnx import rnglib
 from flax.nnx.module import first_from
 import jax
-from jax import lax, random
 import jax.numpy as jnp
 
 
 class DropConnectLinear(nnx.Module):
-    """Custom Linear layer with DropConnect applied to weights during training.
+    """Custom Linear layer with DropConnect applied to weights during training based on :cite:`aminiDeepEvidential2020`.
 
     Attributes:
-        weight: nnx.Param, weight matrix of shape.
-        bias: nnx.Param, bias vector of shape.
-        rate: float, the dropconnect probability.
-        deterministic: bool, if false the inputs are masked, whereas if true, no mask
-            is applied and the inputs are returned as is.
+        kernel: nnx.Param, weight matrix of the layer.
+        bias: nnx.Param, bias of the layer.
+        in_features: int, number of input features.
+        out_features: int, number of output features.
+        use_bias: bool, whether to add bias to the output.
+        dtype: typing.Optional[flax.typing.Dtype], the dtype of the computation (default: infer from input and params).
+        param_dtype: flax.typing.Dtype, the dtype passed to parameter initializers.
+        precision: flax.typing.PrecisionLike, numerical precision of the computation see ``jax.lax.Precision``
+            for details.
+        kernel_init: flax.typing.Initializer, initializer function for the weight matrix.
+        bias_init: flax.typing.Initializer, initializer function for the bias.
+        dot_general: flax.typing.DotGeneralT, dot product function.
+        promote_dtype: flax.typing.PromoteDtypeFn, function to promote the dtype of the arrays to the desired
+            dtype. The function should accept a tuple of ``(inputs, kernel, bias)``
+            and a ``dtype`` keyword argument, and return a tuple of arrays with the
+            promoted dtype.
+        preferred_element_type: flax.typing.Dtype, Optional parameter controls the data type output by
+            the dot product. This argument is passed to ``dot_general`` function.
+            See ``jax.lax.dot`` for details.
+        rate: float, probability of dropping individual weights.
+        deterministic: bool, if false the inputs are scaled by ``1/(1-rate)`` and
+            masked, whereas if true, no mask is applied and the inputs are returned
+            as is.
         rng_collection: str, the rng collection name to use when requesting a rng key.
-        rngs: nnx.Rngs or nnx.RngStream or None, rng key.
-
+        rngs: rnglib.Rngs or rnglib.RngStream or None, rng key.
     """
 
     def __init__(
@@ -40,9 +56,21 @@ class DropConnectLinear(nnx.Module):
             rng_collection: str, rng collection name to use when requesting a rng key.
             rngs: nnx.Rngs or nn.RngStream or None, rng key.
         """
-        self.weight = base_layer.kernel
+        self.kernel = base_layer.kernel
         self.bias = base_layer.bias if base_layer.bias is not None else None
+        self.in_features = base_layer.in_features
+        self.out_features = base_layer.out_features
+        self.use_bias = base_layer.use_bias
+        self.dtype = base_layer.dtype
+        self.param_dtype = base_layer.param_dtype
+        self.precision = base_layer.precision
+        self.kernel_init = base_layer.kernel_init
+        self.bias_init = base_layer.bias_init
+        self.dot_general = base_layer.dot_general
+        self.promote_dtype = base_layer.promote_dtype
+        self.preferred_element_type = base_layer.preferred_element_type
         self.rate = rate
+        self.deterministic: bool = False
         self.rng_collection = rng_collection
 
         if isinstance(rngs, rnglib.Rngs):
@@ -65,7 +93,7 @@ class DropConnectLinear(nnx.Module):
         """Forward pass of the DropConnectLinear layer.
 
         Args:
-           inputs: jax.Array, input data that should be randomly masked.
+           inputs: jax.Array, input data.
            deterministic: bool, if false the inputs are masked, whereas if true, no mask
             is applied and the inputs are returned as is.
            rngs: nnx.Rngs, nnx.RngStream or jax.Array, optional key used to generate the dropconnect mask.
@@ -73,7 +101,14 @@ class DropConnectLinear(nnx.Module):
         Returns:
             jax.Array, layer output.
         """
-        self.deterministic = deterministic
+        kernel = self.kernel[...]
+        bias = self.bias[...] if self.bias is not None else None
+
+        inputs, kernel, bias = self.promote_dtype((inputs, kernel, bias), dtype=self.dtype)
+
+        dot_general_kwargs = {}
+        if self.preferred_element_type is not None:
+            dot_general_kwargs["preferred_element_type"] = self.preferred_element_type
 
         deterministic = first_from(
             deterministic,
@@ -82,19 +117,11 @@ class DropConnectLinear(nnx.Module):
                 as either a __call__ argument or class attribute.""",
         )
 
-        if (self.rate == 0.0) or deterministic:
-            return inputs
-
-        # Prevent gradient NaNs in 1.0 edge-case.
-        if self.rate == 1.0:
-            out = inputs @ jnp.zeros_like(self.weight.value)
-            return out if self.bias is None else out + self.bias
-
         rngs = first_from(
             rngs,
             self.rngs,
-            error_msg="""`deterministic` is False, but no `rngs` argument was provided
-                to DropConnect as either a __call__ argument or class atribute.""",
+            error_msg="""No `rngs` argument was provided to DropConnect
+                as either a __call__ argument or class attribute.""",
         )
 
         if isinstance(rngs, rnglib.Rngs):
@@ -107,24 +134,36 @@ class DropConnectLinear(nnx.Module):
             msg = f"rngs must be Rngs, RngStream or jax.Array, but got {type(rngs)}."
             raise TypeError(msg)
 
-        keep_prob = 1.0 - self.rate
-        mask = random.bernoulli(key, p=keep_prob, shape=self.weight.value.shape)
-        masked_weight = lax.select(mask, self.weight.value / keep_prob, jnp.zeros_like(self.weight.value))
+        if not deterministic:
+            mask = jax.random.bernoulli(key=key, p=(1 - self.rate), shape=kernel.shape)
+            masked_kernel = kernel * mask  # Apply DropConnect
+        else:
+            masked_kernel = kernel * (1 - self.rate)  # Scale weights at interference time
 
-        out = inputs @ masked_weight
+        out = self.dot_general(
+            inputs,
+            masked_kernel,
+            (((inputs.ndim - 1,), (0,)), ((), ())),
+            precision=self.precision,
+            **dot_general_kwargs,
+        )
         if self.bias is not None:
-            out = out + self.bias.value
+            out += jnp.reshape(bias, (1,) * (out.ndim - 1) + (-1,))
         return out
 
-    def __repr__(self) -> str:
-        """Return a string representation of the layer including its class name and key attributes."""
-        return f"{self.__class__.__name__}({self.extra_repr()})"
+    def set_mode(  # noqa: D417
+        self,
+        deterministic: bool | None = None,
+        **kwargs,  # noqa: ANN003
+    ) -> dict:
+        """Class method used by ``nnx.set_mode``.
 
-    def extra_repr(self) -> str:
-        """Expose description of in- and out-features of this layer."""
-        in_features = self.weight.value.shape[0]
-        out_features = self.weight.value.shape[1]
-        return f"in_features={in_features}, out_features={out_features}, bias={self.bias is not None}"
+        Args:
+            deterministic: if True, disables dropconnect masking.
+        """
+        if deterministic is not None:
+            self.deterministic = deterministic
+        return kwargs
 
 
 class BatchEnsembleLinear(nnx.Linear):
