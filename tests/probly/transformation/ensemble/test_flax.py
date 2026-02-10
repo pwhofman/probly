0f128f4b6341b7800285af486602e3e828ce4d9e
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2026-02-10 22:31:36 +0100

    Rename key to seed

--- tests/probly/transformation/ensemble/test_flax.py
+++ tests/probly/transformation/ensemble/test_flax.py
@@ -43,8 +43,8 @@ class TestEnsembleAttributes:
     def test_ensemble_attributes_with_reset(self, flax_model_small_2d_2d) -> None:
         """Tests if the member attributes are not inherited from the base model."""
         num_members = 2
-        key = 2
-        ensemble_model = ensemble(flax_model_small_2d_2d, num_members=2, reset_params=True, key=key)
+        seed = 2
+        ensemble_model = ensemble(flax_model_small_2d_2d, num_members=2, reset_params=True, seed=seed)
 
         assert ensemble_model is not None
         assert isinstance(ensemble_model, nnx.List)
@@ -122,6 +122,29 @@ class TestEnsembleGeneration:
             count_module_member = count_layers(member, nnx.Module)
             assert count_module_member == count_module_original
 
+    def test_rngs_reset(self, flax_dropout_model) -> None:
+        num_members = 1
+        rngs = nnx.Rngs(0, params=1, dropout=2)
+        ensemble_model = ensemble(flax_dropout_model, num_members=num_members, reset_params=True, rngs=rngs)
+
+        assert ensemble_model is not None
+        assert isinstance(ensemble_model, nnx.List)
+        assert len(ensemble_model) == num_members
+
+        # simulate rngs
+        new_rngs = nnx.Rngs(0, params=1, dropout=2)
+        kernel1_key = new_rngs.params()
+        kernel_init = jax.nn.initializers.lecun_normal()
+        linear1_kernel = kernel_init(kernel1_key, (2, 2))
+        _ = new_rngs.params()  # linear1 bias
+        dropout_rngs = new_rngs["dropout"].fork()
+        kernel2_key = new_rngs.params()
+        linear2_kernel = kernel_init(kernel2_key, (2, 2))
+
+        assert jnp.equal(ensemble_model[0].layers[0].kernel.value, linear1_kernel).all()
+        assert ensemble_model[0].layers[1].rngs.key.value == dropout_rngs.key.value
+        assert jnp.equal(ensemble_model[0].layers[2].kernel.value, linear2_kernel).all()
+
 
 class TestEnsembleCalls:
     """Test class for ensemble model calls."""
@@ -140,8 +163,8 @@ class TestEnsembleCalls:
 
     def test_ensemble_flax_custom_model_call_with_reset(self, flax_custom_model) -> None:
         num_members = 2
-        key = 2
-        ensemble_model = ensemble(flax_custom_model, num_members=num_members, reset_params=True, key=key)
+        seed = 2
+        ensemble_model = ensemble(flax_custom_model, num_members=num_members, reset_params=True, seed=seed)
 
         x = jnp.ones((2, 1, 10))
         custom_model_out = flax_custom_model(x)
