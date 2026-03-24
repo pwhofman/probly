00786a1d173178d18562bd0963a9972fcb730999
Paul Hofman <paulwhofman@hotmail.com>
2026-03-24 20:06:39 +0100

    Fix typing DeepEnsemble

--- src/probly/methods/deep_ensemble.py
+++ src/probly/methods/deep_ensemble.py
@@ -13,14 +13,14 @@ from probly.representation.sampling.sampler import EnsembleSampler
 from probly.transformation import ensemble
 
 
-class DeepEnsemble[In, KwIn, Out]:
+class DeepEnsemble[**In, Out]:
     """Deep ensemble class."""
 
-    def __init__(self, base: Predictor[In, KwIn, Out], num_members: int) -> None:
+    def __init__(self, base: Predictor[In, Out], num_members: int) -> None:
         """Initialize the deep ensemble method."""
-        self.ensemble = ensemble(base, num_members=num_members)  # type: ignore[assignment]
+        self.ensemble = ensemble(base, num_members=num_members)
         self._sampler = EnsembleSampler(self.ensemble, sample_factory=create_sample)
 
-    def predict(self, *args: In, **kwargs: KwIn) -> Sample[Out]:
+    def predict(self, *args: In.args, **kwargs: In.kwargs) -> Sample[Out]:
         """Predict a sample for the given input."""
         return self._sampler.sample(*args, **kwargs)
