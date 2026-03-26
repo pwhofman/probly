59aee1d4504b6d90a603cbbaf6209a621e0cb894
Timo <t.loehr@lmu.de>
2026-03-26 18:36:33 +0100

    Use traversers instead of generators to create efficient credal prediction. Add benchmark code as well

--- src/probly/transformation/efficient_credal_prediction/__init__.py
+++ src/probly/transformation/efficient_credal_prediction/__init__.py
@@ -2,6 +2,14 @@
 
 from __future__ import annotations
 
+from probly.lazy_types import TORCH_MODULE
+
 from . import common
 
 efficient_credal_prediction = common.efficient_credal_prediction
+
+
+## Torch
+@common.efficient_credal_prediction_traverser.delayed_register(TORCH_MODULE)
+def _(_: type) -> None:
+    from . import torch as torch  # noqa: PLC0415
--- src/probly/transformation/efficient_credal_prediction/common.py
+++ src/probly/transformation/efficient_credal_prediction/common.py
@@ -4,12 +4,26 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from probly.traverse_nn import nn_compose
+from pytraverse import TRAVERSE_REVERSED, GlobalVariable, lazydispatch_traverser, traverse
+
 if TYPE_CHECKING:
+    from lazy_dispatch import LazyType
     from probly.predictor import Predictor
+    from pytraverse.composition import RegisteredLooseTraverser
+
+efficient_credal_prediction_traverser = lazydispatch_traverser[object](name="efficient_credal_prediction_traverser")
+
+LAST_LAYER = GlobalVariable[bool]("LAST_LAYER", default=True)
 
 
-def efficient_credal_prediction[**In, Out](base: Predictor[In, Out]) -> Predictor[In, Out]:
-    """Create a efficient credal predictor from a base predictor based on :cite:`hofmanefficient`.
+def register(cls: LazyType, traverser: RegisteredLooseTraverser) -> None:
+    """Register a class to be prepended by Dropout layers."""
+    efficient_credal_prediction_traverser.register(cls=cls, traverser=traverser, vars={"last_layer": LAST_LAYER})
+
+
+def efficient_credal_prediction[T: Predictor](base: T) -> T:
+    """Create an efficient credal predictor from a base predictor based on :cite:`hofmanefficient`.
 
     Args:
         base: Predictor, The base model to be used for the efficient credal predictor.
@@ -17,4 +31,6 @@ def efficient_credal_prediction[**In, Out](base: Predictor[In, Out]) -> Predicto
     Returns:
         Predictor, The efficient credal predictor.
     """
-    return base
+    return traverse(
+        base, nn_compose(efficient_credal_prediction_traverser), init={LAST_LAYER: True, TRAVERSE_REVERSED: True}
+    )
