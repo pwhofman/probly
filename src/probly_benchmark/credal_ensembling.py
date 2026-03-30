d247e5a1816a1f49a0bda3cf444d7870df000d45
Paul Hofman <paulwhofman@hotmail.com>
2026-03-30 15:36:30 +0200

    Add logging

--- src/probly_benchmark/credal_ensembling.py
+++ src/probly_benchmark/credal_ensembling.py
@@ -2,15 +2,20 @@
 
 from __future__ import annotations
 
+import logging
+
 import torch
 
 from probly.method.credal_ensembling import credal_ensembling
 from probly.representer.representer import representer
 from probly_benchmark.models import LeNet
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 model = LeNet(n_classes=5)
 cep = credal_ensembling(model, num_members=10)
 rep = representer(cep)
 inputs = torch.randn(3, 1, 28, 28)
 output = rep.predict(inputs)
-print(output)
+logger.info(output)
