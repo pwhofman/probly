2162fb971f68b4818a7e5869313a5c6441be068d
Elias Barth <160789026+elias0224@users.noreply.github.com>
2026-04-15 18:57:08 +0200

    change het_nets return to HetNetsPredictor

--- src/probly/method/het_nets/_common.py
+++ src/probly/method/het_nets/_common.py
@@ -7,7 +7,7 @@ from typing import Protocol, runtime_checkable
 from probly.method.method import predictor_transformation
 from probly.predictor import Predictor, RandomPredictor
 from probly.traverse_nn import nn_compose
-from pytraverse import TRAVERSE_REVERSED, GlobalVariable, lazydispatch_traverser, traverse
+from pytraverse import TRAVERSE_REVERSED, GlobalVariable, lazydispatch_traverser, traverse, CLONE
 
 
 @runtime_checkable
@@ -25,16 +25,16 @@ IS_PARAMETER_EFFICIENT = GlobalVariable[bool]("IS_PARAMETER_EFFICIENT")
 MULTILABEL = GlobalVariable[bool]("MULTILABEL")
 
 
-@predictor_transformation(permitted_predictor_types=None, preserve_predictor_type=False)
+@predictor_transformation(permitted_predictor_types=None, preserve_predictor_type=False) # ty:ignore[invalid-argument-type]
 @HetNetsPredictor.register_factory
-def het_nets[T: Predictor](
-    base: T,
+def het_nets[**In, Out](
+    base: Predictor[In, Out],
     num_factors: int = 10,
     temperature: float = 1.0,
     num_mc_samples: int = 10,
     is_parameter_efficient: bool = False,
     multilabel: bool = False,
-) -> T:
+) -> HetNetsPredictor[In, Out]:
     """Create a HetNets predictor from a base predictor base on :cite:`collier2021hetnets`.
 
     Args:
@@ -52,6 +52,7 @@ def het_nets[T: Predictor](
         base,
         nn_compose(het_nets_traverser),
         init={
+            CLONE: True,
             LAST_LAYER: True,
             TRAVERSE_REVERSED: True,
             NUM_FACTORS: num_factors,
