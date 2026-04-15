739a6ff14ee3d3b06c28df4857b40d8f15a63b07
Advueu963 <S.Thies@campus.lmu.de>
2026-04-15 19:19:31 +0200

    Incorporate set_construction logic of each score into conformal representer

--- src/probly/representer/conformal/__init__.py
+++ src/probly/representer/conformal/__init__.py
@@ -1,26 +1,9 @@
 """This module contains the conformal representers for regression and classification."""
 
-from probly.lazy_types import TORCH_TENSOR, TORCH_TENSOR_LIKE
-
 from ._common import (
-    ConformalClassificationRepresenter,
-    ConformalQuantileRegressionRepresenter,
-    ConformalRegressionRepresenter,
     ConformalRepresenter,
-    flatten_ensemble_quantile_sample,
-    flatten_sample,
 )
 
-
-@flatten_sample.delayed_register((TORCH_TENSOR, TORCH_TENSOR_LIKE))
-@flatten_ensemble_quantile_sample.delayed_register((TORCH_TENSOR, TORCH_TENSOR_LIKE))
-def _(_: type) -> None:
-    from . import torch as torch  # noqa: PLC0415
-
-
 __all__ = [
-    "ConformalClassificationRepresenter",
-    "ConformalQuantileRegressionRepresenter",
-    "ConformalRegressionRepresenter",
     "ConformalRepresenter",
 ]
--- src/probly/representer/conformal/_common.py
+++ src/probly/representer/conformal/_common.py
@@ -2,13 +2,19 @@
 
 from __future__ import annotations
 
+import copy
 from typing import TYPE_CHECKING, Any, override
 
 from lazy_dispatch.singledispatch import lazydispatch
-from probly.conformal.methods.classification._common import ConformalClassificationCalibrator
-from probly.conformal.methods.quantile_regression._common import ConformalQuantileRegressionCalibrator
-from probly.conformal.methods.regression._common import ConformalRegressionCalibrator
-from probly.conformal.utils import is_conformal_calibrated
+from probly.calibrator._common import ConformalCalibrator
+from probly.conformal_scores._common import (
+    AbsoluteErrorScore,
+    ClassificationNonConformityScore,
+    CQRrScore,
+    CQRScore,
+    UACQRScore,
+)
+from probly.predictor import ProbabilisticClassifier
 from probly.predictor._common import (
     predict_raw,
 )
@@ -19,6 +25,7 @@ from probly.representation.conformal_set._common import (
     create_onehot_conformal_set,
 )
 from probly.representation.sample import create_sample
+from probly.representation.sample._common import Sample
 from probly.representation.sample.array import ArraySample
 from probly.representer import representer
 from probly.representer._representer import Representer
@@ -36,89 +43,86 @@ if TYPE_CHECKING:
     from probly.representation.sample._common import Sample
 
 
+def is_conformal_calibrated(calibrator: ConformalCalibrator[Any, Any]) -> bool:
+    """Check if a conformal calibrator has been calibrated."""
+    conformal_quantile = getattr(calibrator, "conformal_quantile", None)
+    non_conformity_score = getattr(calibrator, "non_conformity_score", None)
+    return conformal_quantile is not None and non_conformity_score is not None
+
+
+def dispatch_on_score(predictor: ConformalCalibrator[Any, Any], *args: Any, **kwargs: Any) -> type:
+    """Dispatch on the type of the predictor."""
+    return predictor.non_conformity_score
+
+
+@lazydispatch(dispatch_on=dispatch_on_score)
+def predict_set(predictor: ConformalCalibrator[Any, Any], predictions: Sample[Any]) -> ConformalSet:
+    """Predict a conformal set for the given predictions."""
+    msg = f"Prediction not implemented for type {type(predictor)}."
+    raise NotImplementedError(msg)
+
+
+@representer.register(ConformalCalibrator)
 class ConformalRepresenter[**In, Out: ConformalSet](Representer[Any, In, Out, ConformalSet]):
     predictor: ConformalCalibrator[In, Out]
 
-
-@representer.register(ConformalClassificationCalibrator)
-class ConformalClassificationRepresenter[**In, Out: OneHotConformalSet](ConformalRepresenter[In, Out]):
-    predictor: ConformalClassificationCalibrator[In, Out]
-
-    @override
-    def represent(self, *args: In.args, **kwargs: In.kwargs) -> OneHotConformalSet:
-        """Represent the output of a conformal classification predictor as a one-hot conformal set."""
-        if not is_conformal_calibrated(self.predictor):
-            msg = "The model must first be calibrated before it can be represented."
-            raise ValueError(msg)
-        predictions = predict_raw(self.predictor, *args, **kwargs)
-        non_conformity_score = self.predictor.non_conformity_score(predictions)
-        set_prediction = non_conformity_score <= self.predictor.conformal_quantile
-        return create_onehot_conformal_set(set_prediction)
-
-
-@lazydispatch
-def flatten_sample[T](sample: Sample[T]) -> T:
-    """Flatten a sample to a one-dimensional array for regression interval construction."""
-    msg = f"Flattening not implemented for type {type(sample)}."
-    raise NotImplementedError(msg)
-
-
-@flatten_sample.register(ArraySample)
-def flatten_sample_numpy(sample: ArraySample) -> np.ndarray:
-    raw_array = sample.array
-    if raw_array.ndim == 1:
-        return raw_array
-    if raw_array.ndim == 2 and (raw_array.shape[1] == 1 or raw_array.shape[0] == 1):
-        return raw_array.flatten()
-    return sample.sample_mean()
-
-
-@representer.register(ConformalRegressionCalibrator)
-class ConformalRegressionRepresenter[**In, Out: ConformalSet](ConformalRepresenter[In, Out]):
-    predictor: ConformalRegressionCalibrator[In, Out]
-
     @override
     def represent(self, *args: In.args, **kwargs: In.kwargs) -> ConformalSet:
-        """Represent the output of a conformal regression predictor as an interval conformal set."""
+        """Represent the output of a conformal predictor as a conformal set."""
         if not is_conformal_calibrated(self.predictor):
             msg = "The model must first be calibrated before it can be represented."
             raise ValueError(msg)
-        predictions = create_sample(predict_raw(self.predictor, *args, **kwargs))
-        predictions = flatten_sample(predictions)
-        weight_low, weight_high = self.predictor.non_conformity_score.weight(predictions)
-        low_bound = predictions - weight_low * self.predictor.conformal_quantile
-        upper_bound = predictions + weight_high * self.predictor.conformal_quantile
-        return create_interval_conformal_set(low_bound, upper_bound)
+        # We dispatch
+        set_prediction = predict_set(copy.deepcopy(self.predictor), *args, **kwargs)
+        return set_prediction
 
 
-@lazydispatch
-def flatten_ensemble_quantile_sample[T](sample: Sample[T]) -> T:
-    """Flatten an ensemble quantile sample to a two-column array for interval construction."""
-    msg = f"Flattening not implemented for type {type(sample)}."
-    raise NotImplementedError(msg)
+@predict_set.register(ClassificationNonConformityScore)
+def predict_set_classification(predictor: ConformalCalibrator[Any, Any], *args, **kwargs: Any) -> OneHotConformalSet:
+    """Standard conformal classification prediction."""
+    # We register the predictor as a probabilistic classifier to obtain the raw predictions for the conformal set construction.
+    ProbabilisticClassifier.register_instance(predictor)
+    predictions = predict_raw(predictor, *args, **kwargs)
+    non_conformity_score = predictor.non_conformity_score(predictions)
+    set_prediction = non_conformity_score <= predictor.conformal_quantile
+    return create_onehot_conformal_set(set_prediction)
 
 
-@flatten_ensemble_quantile_sample.register(ArraySample)
-def flatten_ensemble_quantile_sample_numpy(sample: ArraySample) -> np.ndarray:
-    raw_array = sample.array
-    if raw_array.ndim == 3:
-        return sample.sample_mean()
-    return raw_array
+@predict_set.register(AbsoluteErrorScore)
+def predict_set_regression(predictor: ConformalCalibrator[Any, Any], *args, **kwargs: Any) -> ConformalSet:
+    """Standard conformal regression prediction."""
+    predictions = predict_raw(predictor, *args, **kwargs)
+    low_bound = predictions - predictor.conformal_quantile
+    upper_bound = predictions + predictor.conformal_quantile
+    return create_interval_conformal_set(low_bound, upper_bound)
 
 
-@representer.register(ConformalQuantileRegressionCalibrator)
-class ConformalQuantileRegressionRepresenter[**In, Out: ConformalSet](ConformalRepresenter[In, Out]):
-    predictor: ConformalQuantileRegressionCalibrator[In, Out]
+@predict_set.register(CQRScore)
+def predict_set_cqr(predictor: ConformalCalibrator[Any, Any], *args, **kwargs: Any) -> ConformalSet:
+    """Conformalized quantile regression prediction."""
+    predictions = predict_raw(predictor, *args, **kwargs)
+    low_bound = predictions[:, 0] - predictor.conformal_quantile
+    upper_bound = predictions[:, 1] + predictor.conformal_quantile
+    return create_interval_conformal_set(low_bound, upper_bound)
 
-    @override
-    def represent(self, *args: In.args, **kwargs: In.kwargs) -> ConformalSet:
-        """Represent the output of a conformal quantile regression predictor as an interval conformal set."""
-        if not is_conformal_calibrated(self.predictor):
-            msg = "The model must first be calibrated before it can be represented."
-            raise ValueError(msg)
-        predictions = create_sample(predict_raw(self.predictor, *args, **kwargs), sample_axis=0)
-        weight_low, weight_high = self.predictor.non_conformity_score.weight(predictions)
-        mean_predictions = flatten_ensemble_quantile_sample(predictions)
-        low_bound = mean_predictions[:, 0] - self.predictor.conformal_quantile * weight_low
-        upper_bound = mean_predictions[:, 1] + self.predictor.conformal_quantile * weight_high
-        return create_interval_conformal_set(low_bound, upper_bound)
+
+@predict_set.register(CQRrScore)
+def predict_set_cqr_r(predictor: ConformalCalibrator[Any, Any], *args, **kwargs: Any) -> ConformalSet:
+    """Conformalized quantile regression with residuals prediction."""
+    predictions = predict_raw(predictor, *args, **kwargs)
+    width = predictions[:, 1] - predictions[:, 0]
+    low_bound = predictions[:, 0] - predictor.conformal_quantile * width
+    upper_bound = predictions[:, 1] + predictor.conformal_quantile * width
+    return create_interval_conformal_set(low_bound, upper_bound)
+
+
+@predict_set.register(UACQRScore)
+def predict_set_uacqr(predictor: ConformalCalibrator[Any, Any], *args, **kwargs: Any) -> ConformalSet:
+    """Uncertainty-aware conformalized quantile regression prediction."""
+    predictions = predict_raw(predictor, *args, **kwargs)
+    mean_predictions = predictions.sample_mean()
+    standard_deviation = predictions.sample_std()
+    weight_low, weight_high = standard_deviation[:, 0], standard_deviation[:, 1]
+    low_bound = mean_predictions[:, 0] - predictor.conformal_quantile * weight_low
+    upper_bound = mean_predictions[:, 1] + predictor.conformal_quantile * weight_high
+    return create_interval_conformal_set(low_bound, upper_bound)
