68393548ed9936fe705a7556f8455f22db0a4a77
Elias Barth <160789026+elias0224@users.noreply.github.com>
2026-04-23 15:09:06 +0200

    update benchmark to match representation and quantification

--- src/probly_benchmark/method_scripts/het_nets.py
+++ src/probly_benchmark/method_scripts/het_nets.py
@@ -8,7 +8,7 @@ import torch
 
 from probly.method.het_nets import het_nets
 from probly.quantification import quantify
-from probly.representer import Sampler
+from probly.representer import representer
 from probly_benchmark.models import LeNet
 
 logging.basicConfig(level=logging.INFO)
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 model = LeNet(n_classes=5)
 cep = het_nets(model, predictor_type="probabilistic_classifier")
-rep = Sampler(cep, num_samples=10)
+rep = representer(cep, num_samples=10)
 logger.info(rep)
 inputs = torch.randn(3, 1, 28, 28)
 logger.info(cep(inputs))
@@ -25,4 +25,4 @@ logger.info(output)
 logger.info(output.shape)
 quantification = quantify(output)
 logger.info(quantification)
-logger.info(quantification.total)  # ty:ignore[unresolved-attribute]
+logger.info(quantification.aleatoric)  # ty:ignore[unresolved-attribute]
