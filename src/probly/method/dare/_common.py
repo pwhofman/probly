49dd7ab6825e7457892757556b33230bfaf90b9f
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2026-04-24 23:21:17 +0200

    Remove args lamda_reg and threshold, change to flexdispatch

--- src/probly/method/dare/_common.py
+++ src/probly/method/dare/_common.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
-from lazy_dispatch import lazydispatch
+from flextype import flexdispatch
 from probly.method.ensemble import EnsemblePredictor
 from probly.method.method import predictor_transformation
 
@@ -17,12 +17,10 @@ class DarePredictor[**In, Out](EnsemblePredictor[In, Out], Protocol):
     """Protocol for dare predictors."""
 
 
-@lazydispatch
+@flexdispatch
 def dare_generator[**In, Out](
     base: Predictor[In, Out],
     num_members: int,
-    lambda_reg: float,
-    threshold: float,
 ) -> DarePredictor[In, Out]:
     """Generate a dare from a base model."""
     msg = f"No dare generator is registered for type {type(base)}"
@@ -36,18 +34,14 @@ def dare_generator[**In, Out](
 def dare[**In, Out](
     base: Predictor[In, Out],
     num_members: int = 1,
-    lambda_reg: float = 0.01,
-    threshold: float = 0.1,
 ) -> DarePredictor[In, Out]:
     """Create a dare predictor from a base predictor based on :cite:`arXiv:2304.04042`.
 
     Args:
         base: Predictor, the base model to be used for dare.
         num_members: The number of members to generate.
-        lambda_reg: The anti-regularization coefficient.
-        threshold: The loss threshold to trigger anti-regularization.
 
     Returns:
         Predictor, The dare predictor.
     """
-    return dare_generator(base, num_members=num_members, lambda_reg=lambda_reg, threshold=threshold)
+    return dare_generator(base, num_members=num_members)
