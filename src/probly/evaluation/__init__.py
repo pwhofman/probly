e41433e39be5866711be494eb205c0e5d8fd4825
Maximilian <maximilian.muschalik@gmail.com>
2026-04-25 17:17:49 +0200

    removed active learning import from evaluation

--- src/probly/evaluation/__init__.py
+++ src/probly/evaluation/__init__.py
@@ -1,13 +1,1 @@
 """Init module for evaluation implementations."""
-
-import importlib
-from types import ModuleType
-
-
-def __getattr__(name: str) -> ModuleType:
-    if name == "active_learning":
-        module = importlib.import_module(f"{__name__}.active_learning")
-        globals()[name] = module
-        return module
-    msg = f"module {__name__!r} has no attribute {name!r}"
-    raise AttributeError(msg)
