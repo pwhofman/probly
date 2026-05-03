518fa740c32ff5ca04fda846d30c4fe04f70ee54
Paul Hofman <paulwhofman@hotmail.com>
2026-05-03 18:41:35 +0200

    Fix for ninco ssb hard

--- src/probly_benchmark/data.py
+++ src/probly_benchmark/data.py
@@ -42,6 +42,7 @@ TRANSFORMS_TEST = {
     ),
     "imagenet": T.Compose(
         [
+            T.ToImage(),
             T.Resize((224, 224), antialias=True),
             T.ToDtype(torch.float32, scale=True),
             T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
