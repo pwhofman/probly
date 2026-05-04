4db599751e8caf26c93706bfad76530365c9fdf0
Clemens Damke <damke@kiml.ifi.lmu.de>
2026-05-04 23:28:36 +0200

    fixed embedding registration

--- src/probly/representation/embedding/__init__.py
+++ src/probly/representation/embedding/__init__.py
@@ -5,7 +5,7 @@ from probly.lazy_types import TORCH_TENSOR, TORCH_TENSOR_LIKE
 from ._common import Embedding, EmbeddingSample, EmbeddingSampleSample, create_embedding
 
 
-@create_embedding.register((TORCH_TENSOR, TORCH_TENSOR_LIKE))
+@create_embedding.delayed_register((TORCH_TENSOR, TORCH_TENSOR_LIKE))
 def _(_: type) -> None:
     from . import torch as torch  # noqa: PLC0415
 
