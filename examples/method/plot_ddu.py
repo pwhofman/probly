4a5bc93c53b58e4c61b2470d1cac2c121f5b8a5a
Celina <celina@surrer.com>
2026-05-18 18:33:38 +0200

    delete plotting_decomp.py and mino changes to plot_ddu.py

--- examples/method/plot_ddu.py
+++ examples/method/plot_ddu.py
@@ -11,45 +11,49 @@ from __future__ import annotations
 from sklearn.datasets import make_moons
 import torch
 from torch import nn
+import numpy as np
 
 from probly.representer import representer
 from probly.method.ddu import ddu
 
-from examples.utils.model import MiniResNet1D
+from examples.utils.model import MLPClassifier, MiniResNet1D
 from examples.utils.plotting import plot_example_uncertainty
 
-# %%
-# 1. Prepare the Two Moons dataset
+#%%
+#1. Prepare the Two Moons dataset
 X, y = make_moons(n_samples=500, noise=0.05, random_state=0)
 X_tensor = torch.from_numpy(X).float()
 y_tensor = torch.from_numpy(y).long()
 
-# %%
-# 2. Set up the base model
-base_model = MiniResNet1D(n_classes=2)
+#%%
+#2. Set up the base model
+base_model = MiniResNet1D()
 
-ddu_model = ddu(base_model, sn_coeff=1.5)
+ddu_model = ddu(base_model, sn_coeff=3.0, predictor_type="logit_classifier")
 
-opt = torch.optim.Adam(base_model.parameters(), lr=1e-3)
+opt = torch.optim.Adam(ddu_model.parameters(), lr=1e-3)
 
-# %%
-# 3. Train the DDU model
+#%%
+#3. Train the DDU model
 ddu_model.train()
-for epoch in range(300):
+for epoch in range(500):
     out = ddu_model(X_tensor)
-    mean_attr = getattr(out, "mean", None)
-    logits = out[0] if isinstance(out, tuple) else (mean_attr if isinstance(mean_attr, torch.Tensor) else out)
+    logits = out[0]
+
     loss = nn.functional.cross_entropy(logits, y_tensor)
 
     opt.zero_grad()
     loss.backward()
     opt.step()
-
-
-# %%
-# 4. Evaluate predictive uncertainty
+#%%
+#4. Fitting Density Head
 ddu_model.eval()
+
+ddu_model.fit_density_head(X_tensor, y_tensor)
+
+#%%
+#5. Evaluate predictive uncertainty
 rep = representer(ddu_model)
 
-plot = plot_example_uncertainty(X_tensor, y, rep, title="DDU Predictive Uncertainty", vmin = None, vmax = None)
+plot = plot_example_uncertainty(X, y, rep, title="DDU Predictive Uncertainty (Corrected)", vmin=None, vmax=None)
 plot.show()
