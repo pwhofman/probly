abaeb9162f670707f2121152072fa636b34c4779
Celina <celina@surrer.com>
2026-05-21 16:43:23 +0200

    added imports to init and deleted unused model

---
+++ examples/utils/__init__.py
@@ -0,0 +1,9 @@
+from .model import MLPClassifier, ResFFN, SequentialModel
+from .plotting import plot_example_uncertainty
+
+__all__ = [
+    "MLPClassifier",
+    "ResFFN",
+    "SequentialModel",
+    "plot_example_uncertainty"
+]
