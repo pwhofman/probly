b82d369a8cf599d691afc0b94bce96d1d836cc31
Tjark Maurice Fromm <maurice.fromm@hotmail.de>
2026-05-22 16:01:15 +0200

    Revert `probly.plot.credal.__init__` and delete `.credal._torch.py`

--- src/probly/plot/credal/__init__.py
+++ src/probly/plot/credal/__init__.py
@@ -1,10 +1,5 @@
 """Credal set plotting (binary interval, ternary simplex, and spider/radar)."""
 
-import contextlib
-
 from .plot import plot_credal_set
-
-with contextlib.suppress(ImportError):
-    from . import _torch  # noqa: F401
 
 __all__ = ["plot_credal_set"]
